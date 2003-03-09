ceea636868a81c8deafba5719a4d5de3cb66ca2e
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-03-09 19:31:47 +0000

    added proper file header
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@29 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/bp.py
+++ src/bp.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+
+### BITPIM
+###
+### Copyright (C) 2003 Roger Binns <rogerb@rogerbinns.com>
+###
+### This software is under the Artistic license.
+### http://www.opensource.org/licenses/artistic-license.php
+###
+### $Id$
+
+
+
 # only gui mode support at the moment
 
 import sys
