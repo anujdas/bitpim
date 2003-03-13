16fae270bd8fd5c5216de36d9dbd19efe348d5a4
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-03-13 05:25:35 +0000

    refresh just the row labels rather than entire grid
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@55 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/guiwidgets.py
+++ src/guiwidgets.py
@@ -232,7 +232,7 @@ class PhoneDataTable(wxPyGridTableBase):
         print "SetValue",row,col,value
         self._data[self.roworder[row]][self.labels[col]]=value
         if self.labels[col]=='name':
-            self.GetView().Refresh()
+            self.GetView().GetGridRowLabelWindow().Refresh()
         self.needswrite=1
 
     def GetColLabelValue(self, col):
