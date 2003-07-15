21a0e81e9541d29ee2cf4154c1c9e36acac880e0
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-07-15 01:23:24 +0000

    rejig things so that makedist can also work on Linux
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@118 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/p2econfig.py
+++ src/p2econfig.py
@@ -1,26 +1,8 @@
 from distutils.core import setup
 
 import py2exe
-import glob
-import os
+import makedist
 
-tbl={}
-# list of file extensions
-exts=[ 'exe', 'dll', 'txt', 'png', 'ttf', 'wav']
-# list of directories to look in
-dirs=[ '.\\resources' , '.' ]
-for ext in exts:
-    for dir in dirs:
-        for file in glob.glob(dir+"\\*."+ext):
-            if file==".\\resources\\group.png": continue # don't ship it for moment
-            d=os.path.dirname(file)
-            if not tbl.has_key(d):
-                tbl[d]=[]
-            tbl[d].append(file)
 
-files=[]
-for i in tbl.keys():
-    files.append( (i, tbl[i]) )
-
-setup(name="bitpim", scripts=['bp.py'], data_files=files)
+setup(name="bitpim", scripts=['bp.py'], data_files=makedist.resources())
       
