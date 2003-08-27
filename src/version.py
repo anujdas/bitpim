0e5fae1670472ab252479c2002793bc686a45c16
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-08-27 07:13:26 +0000

    - supply extra version meta-information - can generate file for helpblocks macros
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@244 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/version.py
+++ src/version.py
@@ -3,6 +3,8 @@
 
 """Information about BitPim version number"""
 
+import time
+
 name="BitPim"
 version="0.6"
 release=0  # when rereleases of the same version happen, this gets incremented
@@ -23,3 +25,13 @@ if testver>0:
     versionstring+="-test"+`testver`
 if release>0:
     versionstring+="-"+`release`
+
+author="Roger Binns"
+author_email="rogerb@users.sourceforge.net"
+url="http://bitpim.sourceforge.net"
+
+if __name__=='__main__':
+    # generated for the benefit of the help
+    # purposely missing " around values
+    print "#define VERSION", versionstring
+    print "#define DATENOW", time.strftime("%d %B %Y")
