feb257143dcb3264c2b7a1cd3bccf34003393a12
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-08-27 07:13:07 +0000

    supply extra version meta-information
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@243 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/p2econfig.py
+++ src/p2econfig.py
@@ -2,7 +2,13 @@ from distutils.core import setup
 
 import py2exe
 import makedist
+import version
 
-
-setup(name="bitpim", scripts=['bp.py'], data_files=makedist.resources())
+setup(name="bitpim",
+      author=version.author,
+      author_email=version.author_email,
+      url=version.url,
+      version=version.versionstring,
+      scripts=['bp.py'],
+      data_files=makedist.resources())
       
