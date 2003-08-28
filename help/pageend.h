3bd471c2d0effeafcd5e0887f8e9031044c49528
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-08-28 23:18:50 +0000

    lots of help updates
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@252 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ help/pageend.h
@@ -0,0 +1,5 @@
+<hr> 
+#include "navbar.h" 
+#include "version.h"
+<p align=center><b>BitPim</b> VERSION &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; DATENOW</p>
+</body></html>
