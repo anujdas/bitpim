708cd75b74c71cccf97488f09729632d82b04294
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-09-05 06:56:58 +0000

    many many doc updates
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@289 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ help/navbar.h
@@ -0,0 +1,10 @@
+
+#ifdef _HELP_PREVIOUS_FILE						
+TEXTLINK(_HELP_PREVIOUS_FILE,_HELP_PREVIOUS_TITLE,Previous)		
+#endif									
+#ifdef _HELP_PARENT_FILE						
+TEXTLINK(_HELP_PARENT_FILE,_HELP_PARENT_TITLE,Up)			
+#endif									
+#ifdef _HELP_NEXT_FILE							
+TEXTLINK(_HELP_NEXT_FILE,_HELP_NEXT_TITLE,Next)
+#endif
