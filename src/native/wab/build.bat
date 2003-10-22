a1093a26946a3986a32d13805411caa2c85695f7
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-10-22 11:08:20 +0000

    - removed standalone binary stuff since that code doesn't exist anymore - some sed hackery to do my constants
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@438 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/native/wab/build.bat
+++ src/native/wab/build.bat
@@ -1,5 +1,6 @@
-@rem standalone binary
-@rem g++ -g -Wall -I c:\projects\fixedwab -o wab.exe wab.cpp
-@rem swig version
-"c:\program files\swig-1.3.19\swig.exe" -python -c++ -o pywabimpl.cpp pywab.swg
+genprops.py
+"c:\program files\swig-1.3.19\swig.exe" -python -c++ -o _pywabimpl.cpp pywab.swg
+@rem remove our constants hackery
+sed s/constants:://g < _pywabimpl.cpp > pywabimpl.cpp
+@del _pywabimpl.cpp
 g++ -g -Wall -Wl,--enable-auto-import -shared -L c:\python23\libs -I c:\python23\include -I c:\projects\fixedwab -o _pywabimpl.dll pywabimpl.cpp wab.cpp -lpython23
