76b675febd3f33bd7aa457a9f638d1f7d69b8849
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-11-02 05:07:27 +0000

    - guess headers - filters
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@475 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/importexport.py
+++ src/importexport.py
@@ -45,7 +45,15 @@ class ImportCSVDialog(wx.Dialog):
         ',': "Comma"
         }
 
-    possiblecolumns=["<ignore>", "First Name", "Full Name", "Middle Name", "Nickname", "Home Address", "Home City"]
+    possiblecolumns=["<ignore>", "First Name", "Last Name", "Middle Name",
+                     "Name", "Nickname", "Email address", "Home Street",
+                     "Home City", "Home Postal Code", "Home State",
+                     "Home Country/Region",  "Home Phone", "Home Fax", "Mobile Phone", "Home Web Page",
+                     "Business Street", "Business City", "Business Postal Code",
+                     "Business State", "Business Country/Region", "Business Web Page",
+                     "Business Phone", "Business Fax", "Pager", "Company", "Notes"]
+    
+    
     columneditor=wx.grid.GridCellChoiceEditor(possiblecolumns, False)
 
     def __init__(self, filename, parent, id, title, style=wx.CAPTION|\
@@ -63,6 +71,8 @@ class ImportCSVDialog(wx.Dialog):
         self.data=DSV.organizeIntoLines(self.rawdata, textQualifier=self.qualifier)
         self.delimiter= DSV.guessDelimiter(self.data)
         self.data=DSV.importDSV(self.data, delimiter=self.delimiter, textQualifier=self.qualifier)
+        self.FigureOutColumns()
+        # Delimter and Qualifier row
         hbs=wx.BoxSizer(wx.HORIZONTAL)
         hbs.Add(wx.StaticText(self, -1, "Delimiter"), 0, wx.EXPAND|wx.ALL, 2)
         self.wdelimiter=wx.ComboBox(self, wx.NewId(), self.PrettyDelimiter(self.delimiter), choices=self.delimiternames.values())
@@ -71,21 +81,35 @@ class ImportCSVDialog(wx.Dialog):
         self.wqualifier=wx.ComboBox(self, wx.NewId(), self.qualifier, choices=['"', "'", "(None)"])
         hbs.Add(self.wqualifier, 1, wx.EXPAND|wx.ALL, 2)
         vbs.Add(hbs, 0, wx.EXPAND|wx.ALL, 5)
+        # Pre-set columns and save row
         hbs=wx.BoxSizer(wx.HORIZONTAL)
         hbs.Add(wx.StaticText(self, -1, "Columns"), 0, wx.EXPAND|wx.ALL, 2)
         self.wcolumnsname=wx.ComboBox(self, wx.NewId(), "Custom", choices=self.predefinedcolumns+["Custom"], style=wx.CB_READONLY|wx.CB_DROPDOWN)
-        self.columns=[]
         hbs.Add(self.wcolumnsname, 1, wx.EXPAND|wx.ALL, 2)
         self.wsave=wx.CheckBox(self, wx.NewId(), "Save")
-        self.wsave.SetValue(True)
+        self.wsave.SetValue(False)
         hbs.Add(self.wsave, 0, wx.EXPAND|wx.ALL, 2)
         vbs.Add(hbs, 0, wx.EXPAND|wx.ALL, 5)
+        # Only records with ... row
+        hbs=wx.BoxSizer(wx.HORIZONTAL)
+        hbs.Add(wx.StaticText(self, -1, "Only rows with "), 0, wx.EXPAND|wx.ALL,2)
+        self.wname=wx.CheckBox(self, wx.NewId(), "a name")
+        hbs.Add(self.wname, 0, wx.EXPAND|wx.LEFT|wx.RIGHT,7)
+        self.wnumber=wx.CheckBox(self, wx.NewId(), "a number")
+        hbs.Add(self.wnumber, 0, wx.EXPAND|wx.LEFT|wx.RIGHT,7)
+        self.waddress=wx.CheckBox(self, wx.NewId(), "an address")
+        hbs.Add(self.waddress, 0, wx.EXPAND|wx.LEFT|wx.RIGHT,7)
+        self.wemail=wx.CheckBox(self, wx.NewId(), "an email")
+        hbs.Add(self.wemail, 0, wx.EXPAND|wx.LEFT,7)
+        vbs.Add(hbs,0, wx.EXPAND|wx.ALL,5)
+        # Preview grid row
         self.preview=PreviewGrid(self, wx.NewId())
         self.preview.CreateGrid(10,10)
         self.preview.SetColLabelSize(0)
         self.preview.SetRowLabelSize(0)
         self.preview.SetMargins(1,0)
         vbs.Add(self.preview, 1, wx.EXPAND|wx.ALL, 5)
+        # Static line and buttons
         vbs.Add(wx.StaticLine(self, -1, style=wx.LI_HORIZONTAL), 0, wx.EXPAND|wx.ALL,5)
         vbs.Add(self.CreateButtonSizer(wx.OK|wx.CANCEL|wx.HELP), 0, wx.ALIGN_CENTER|wx.ALL, 5)
         wx.CallAfter(self.FillPreview)
@@ -93,7 +117,6 @@ class ImportCSVDialog(wx.Dialog):
         self.SetAutoLayout(True)
                 
     def FillPreview(self):
-        wx.Yield()
         wx.BeginBusyCursor(wx.StockCursor(wx.CURSOR_ARROWWAIT))
         self.preview.BeginBatch()
         self.preview.DeleteCols(0,self.preview.GetNumberCols())
@@ -112,12 +135,46 @@ class ImportCSVDialog(wx.Dialog):
             self.columns.append("<ignore>")
         self.columns=self.columns[:numcols]
         for col in range(numcols):
+            if col==3:
+                print self.columns[3]
             self.preview.SetCellValue(0, col, self.columns[col])
             self.preview.SetCellEditor(0, col, self.columneditor)
-        self.preview.AutoSizeColumns(True)
-        self.preview.AutoSizeRows(True)
+        self.preview.AutoSizeColumns()
+        self.preview.AutoSizeRows()
         self.preview.EndBatch()
         wx.EndBusyCursor()
+
+    def FigureOutColumns(self):
+        "Initialize the colums variable, using header row if there is one"
+        numcols=max(map(lambda x: len(x), self.data))
+        if not hasattr(self, "columns"):
+            self.columns=["<ignore>"]*numcols
+        if not DSV.guessHeaders(self.data):
+            return
+        while len(self.columns)<numcols:
+            self.columns.append("<ignore>")
+        self.columns=self.columns[:numcols]
+        headers=self.data[0]
+        self.data=self.data[1:]
+        mungedcolumns=[]
+        for c in self.possiblecolumns:
+            mungedcolumns.append("".join(filter(lambda x: x in "abcdefghijklmnopqrstuvwxyz0123456789", c.lower())))
+        # look for header in possible columns
+        for col,header in zip(range(numcols), headers):
+            if header in self.possiblecolumns:
+                self.columns[col]=header
+                continue
+            h="".join(filter(lambda x: x in "abcdefghijklmnopqrstuvwxyz0123456789", header.lower()))
+            print h
+            
+            if h in mungedcolumns:
+                self.columns[col]=self.possiblecolumns[mungedcolumns.index(h)]
+                print `self.columns[col]`
+                continue
+            else:
+                print mungedcolumns
+        print self.columns
+                
 
     def PrettyDelimiter(self, delim):
         "Returns a pretty version of the delimiter (eg Tab, Space instead of \t, ' ')"
