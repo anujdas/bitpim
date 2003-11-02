7910b9b40cd44ad00afa9de9a6857f09a3cd9bec
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-11-02 00:02:36 +0000

    - build preview table - appropriate editors etc in preview
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@474 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/importexport.py
+++ src/importexport.py
@@ -22,6 +22,20 @@ from DSV import DSV
 # My modules
 import gui
 
+
+class PreviewGrid(wx.grid.Grid):
+
+    def __init__(self, parent, id):
+        wx.grid.Grid.__init__(self, parent, id)
+        wx.grid.EVT_GRID_CELL_LEFT_DCLICK(self, self.OnLeftDClick)
+
+    # (Taken from the demo) I do this because I don't like the default
+    # behaviour of not starting the cell editor on double clicks, but
+    # only a second click.
+    def OnLeftDClick(self, evt):
+        if self.CanEnableCellControl():
+            self.EnableCellEditControl()
+
 class ImportCSVDialog(wx.Dialog):
     "The dialog for importing CSV"
 
@@ -31,8 +45,11 @@ class ImportCSVDialog(wx.Dialog):
         ',': "Comma"
         }
 
+    possiblecolumns=["<ignore>", "First Name", "Full Name", "Middle Name", "Nickname", "Home Address", "Home City"]
+    columneditor=wx.grid.GridCellChoiceEditor(possiblecolumns, False)
+
     def __init__(self, filename, parent, id, title, style=wx.CAPTION|\
-                 wx.SYSTEM_MENU|wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER ):
+                 wx.SYSTEM_MENU|wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.NO_FULL_REPAINT_ON_RESIZE):
         wx.Dialog.__init__(self, parent, id=id, title=title, style=style, size=(640,480))
         self.UpdatePredefinedColumns()
         vbs=wx.BoxSizer(wx.VERTICAL)
@@ -57,9 +74,13 @@ class ImportCSVDialog(wx.Dialog):
         hbs=wx.BoxSizer(wx.HORIZONTAL)
         hbs.Add(wx.StaticText(self, -1, "Columns"), 0, wx.EXPAND|wx.ALL, 2)
         self.wcolumnsname=wx.ComboBox(self, wx.NewId(), "Custom", choices=self.predefinedcolumns+["Custom"], style=wx.CB_READONLY|wx.CB_DROPDOWN)
+        self.columns=[]
         hbs.Add(self.wcolumnsname, 1, wx.EXPAND|wx.ALL, 2)
+        self.wsave=wx.CheckBox(self, wx.NewId(), "Save")
+        self.wsave.SetValue(True)
+        hbs.Add(self.wsave, 0, wx.EXPAND|wx.ALL, 2)
         vbs.Add(hbs, 0, wx.EXPAND|wx.ALL, 5)
-        self.preview=wx.grid.Grid(self, wx.NewId())
+        self.preview=PreviewGrid(self, wx.NewId())
         self.preview.CreateGrid(10,10)
         self.preview.SetColLabelSize(0)
         self.preview.SetRowLabelSize(0)
@@ -67,9 +88,36 @@ class ImportCSVDialog(wx.Dialog):
         vbs.Add(self.preview, 1, wx.EXPAND|wx.ALL, 5)
         vbs.Add(wx.StaticLine(self, -1, style=wx.LI_HORIZONTAL), 0, wx.EXPAND|wx.ALL,5)
         vbs.Add(self.CreateButtonSizer(wx.OK|wx.CANCEL|wx.HELP), 0, wx.ALIGN_CENTER|wx.ALL, 5)
+        wx.CallAfter(self.FillPreview)
         self.SetSizer(vbs)
         self.SetAutoLayout(True)
                 
+    def FillPreview(self):
+        wx.Yield()
+        wx.BeginBusyCursor(wx.StockCursor(wx.CURSOR_ARROWWAIT))
+        self.preview.BeginBatch()
+        self.preview.DeleteCols(0,self.preview.GetNumberCols())
+        self.preview.DeleteRows(0,self.preview.GetNumberRows())
+        self.preview.ClearGrid()
+        numrows=len(self.data)
+        numcols=max(map(lambda x: len(x), self.data))
+        print numrows,numcols
+        self.preview.AppendRows(numrows+1)
+        self.preview.AppendCols(numcols)
+        for row in range(numrows):
+            for col in range(numcols):
+                self.preview.SetCellValue(row+1, col, str(self.data[row][col]))
+                self.preview.SetReadOnly(row+1, col, True)
+        while len(self.columns)<numcols:
+            self.columns.append("<ignore>")
+        self.columns=self.columns[:numcols]
+        for col in range(numcols):
+            self.preview.SetCellValue(0, col, self.columns[col])
+            self.preview.SetCellEditor(0, col, self.columneditor)
+        self.preview.AutoSizeColumns(True)
+        self.preview.AutoSizeRows(True)
+        self.preview.EndBatch()
+        wx.EndBusyCursor()
 
     def PrettyDelimiter(self, delim):
         "Returns a pretty version of the delimiter (eg Tab, Space instead of \t, ' ')"
