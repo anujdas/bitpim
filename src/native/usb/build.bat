94824f3f9395cc0dca7a47f95dc60d362a321fc7
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-11-26 08:13:32 +0000

    - generate less code - made name language neutral
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@604 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/native/usb/build.bat
+++ src/native/usb/build.bat
@@ -2,6 +2,6 @@ set USBDIR=c:\projects\libusb-win32-bin-0.1.7.8
 set SWIG="c:\program files\swig-1.3.19\swig.exe"
 set PYTHONDIR=c:\python23
 
-%SWIG% -I%USBDIR%\include -python libusb.i
+%SWIG% -I%USBDIR%\include -python -no_default libusb.i
 
-gcc -Wall -g -shared -I %USBDIR%\include -L %USBDIR%\lib\gcc -I %PYTHONDIR%\include -L %PYTHONDIR%\libs -o _pylibusb.dll libusb_wrap.c -lusb -lpython23
+gcc -Wall -g -shared -I %USBDIR%\include -L %USBDIR%\lib\gcc -I %PYTHONDIR%\include -L %PYTHONDIR%\libs -o _libusb.dll libusb_wrap.c -lusb -lpython23
