14c72ccbd05f0278df855e76c168c091810cda66
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-12-07 22:07:01 +0000

    make imports possible
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@665 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/native/usb/__init__.py
@@ -0,0 +1,13 @@
+### BITPIM
+###
+### Copyright (C) 2003 Roger Binns <rogerb@rogerbinns.com>
+###
+### This software is under the Artistic license.
+### Please see the accompanying LICENSE file
+###
+### $Id: styles.xy,v 1.4 2003/12/07 06:23:44 rogerb Exp
+
+# Keep python happy that this is a module
+
+import usb
+import libusb
