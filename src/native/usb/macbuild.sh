c7da6b6cf41eb4fa94dc78f5a05882bcbf0659de
n9yty <n9yty@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-12-16 19:46:01 +0000

    Build the libusb wrapper on the Macintosh
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@713 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/native/usb/macbuild.sh
@@ -0,0 +1,7 @@
+PYTHONVER=python2.3
+INCLUDEDIR=/System/Library/Frameworks/Python.framework/Versions/2.3/include/$PYTHONVER
+
+swig -python -I/usr/local/include libusb.i
+
+gcc -Wall -O2  -bundle -undefined suppress -flat_namespace -I $INCLUDEDIR -I /usr/local/include -o _libusb.so libusb_wrap.c -L/usr/local/lib -lusb -framework Python
+
