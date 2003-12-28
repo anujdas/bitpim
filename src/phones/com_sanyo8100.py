364a5c94f1868647d846fdf22d02b781586da1b1
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2003-12-28 04:45:31 +0000

    Conditional code to test out reading of Call History
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@759 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_sanyo8100.py
+++ src/phones/com_sanyo8100.py
@@ -30,6 +30,47 @@ class Phone(com_phone.Phone):
         com_sanyo.Phone.__init__(self, logtarget, commport)
         self.mode=self.MODENONE
 
+    def getcalendar(self,result):
+
+        result=com_sanyo.Phone.getcalendar(self,result)
+# Remove the following line to try out import of call history
+# into BitPim Calendar
+        return result
+
+        calres=result['calendar']
+        count=max(calres)
+        self.log("Calendar has "+`count`+" entries")
+
+        historytypetab=("Outgoing", "Incoming", "Missed")
+        req=self.protocolclass.historyrequest()
+        reqmisc=self.protocolclass.historymiscrequest()
+        for historytype in range(0,3):
+            for i in range(0,20):
+                req.header.command=0x3d+historytype
+                req.slot=i
+                res=self.sendpbcommand(req,self.protocolclass.historyresponse)
+                self.log(historytypetab[historytype]+" Call: "+res.entry.phonenum+"("+res.entry.name+")")
+                entry={}
+                entry['pos']=200+historytype*100+i
+                entry['changeserial']=0
+                datetime=res.entry.date
+                entry['start']=self.decodedate(datetime)
+                entry['end']=entry['start']
+                entry['description']=historytypetab[historytype]+":"+res.entry.phonenum+"("+res.entry.name+")"
+                entry['repeat']=None
+                entry['alarm']=None
+                entry['ringtone']=0
+                entry['snoozedelay']=0
+
+                reqmisc.header.command=0x60+historytype
+                reqmisc.slot=i
+                resmisc=self.sendpbcommand(reqmisc,self.protocolclass.historymiscresponse)
+
+                calres[count]=entry
+                count+=1
+
+        result['calendar']=calres
+        return result
 
 class Profile(com_sanyo.Profile):
 
