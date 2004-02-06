82b53e67ba383d4653fa85e5d335006f78fb4549
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-02-06 13:38:37 +0000

    - added SetFontScale method to html window - added an EasyHtmlPrinting class that is actually easy and unlike the wx one   does things like remember its settings and lets you set the scale
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@903 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/bphtml.py
+++ src/bphtml.py
@@ -35,16 +35,21 @@ class HTMLWindow(wx.html.HtmlWindow):
        - Shift-clicking on a link copies it to the clipboard
     """
     def __init__(self, parent, id, relsize=0.7):
+        wx.html.HtmlWindow.__init__(self, parent, id)
+        wx.EVT_KEY_UP(self, self.OnKeyUp)
+        self.thetext=""
+        self.SetFontScale(relsize)
+
+    def SetFontScale(self, scale):
         # default sizes on windows
         basefonts=[7,8,10,12,16,22,30]
         # defaults on linux
         if guihelper.IsGtk():
             basefonts=[10,13,17,20,23,27,30]
-        wx.html.HtmlWindow.__init__(self, parent, id)
-        wx.EVT_KEY_UP(self, self.OnKeyUp)
-        self.thetext=""
-        if relsize!=1:
-            self.SetFonts("", "", [int(sz*relsize) for sz in basefonts])
+        self.SetFonts("", "", [int(sz*scale) for sz in basefonts])
+        # the html widget clears itself if you set the scale
+        if len(self.thetext):
+            wx.html.HtmlWindow.SetPage(self,self.thetext)
 
 ##    def OnCellMouseHover(self, cell, x, y):
 ##        print cell
@@ -261,6 +266,135 @@ def _applystyle(node, style):
     for i in node.children:
         _applystyle(i, style)
 
+
+class HtmlEasyPrinting:
+    """Similar to wxHtmlEasyPrinting, except this is actually useful.
+
+    The following additions are present:
+
+      - The various settings are saved in a supplied config object
+      - You can set the scale for the fonts, otherwise the default
+        is way too large (you would get about 80 chars across)
+    """
+
+    def __init__(self, parent=None, config=None, configstr=None):
+        self.parent=parent
+        self.config=config
+        self.configstr=configstr
+        self.printData=wx.PrintData()
+        self._configtoprintdata(self.printData)
+        # setup margins
+        self._configtopagesetupdata()
+
+    def SetParentFrame(self, parent):
+        self.parent=parent
+
+    def PrinterSetup(self):
+        printerDialog = wx.PrintDialog(self.parent)
+        printerDialog.GetPrintDialogData().SetPrintData(self.printData)
+        printerDialog.GetPrintDialogData().SetSetupDialog(True)
+        if printerDialog.ShowModal()==wx.ID_OK:
+            self.printData = printerDialog.GetPrintDialogData().GetPrintData()
+            self._printdatatoconfig(self.printData)
+        printerDialog.Destroy()
+
+    def PageSetup(self):
+        psdd=wx.PageSetupDialogData()
+        psdd.SetPrintData(self.printData)
+        self._configtopagesetupdata(psdd)
+        pageDialog=wx.PageSetupDialog(self.parent, psdd)
+        if pageDialog.ShowModal()==wx.ID_OK:
+            self.printData = pageDialog.GetPageSetupData().GetPrintData()
+            self._printdatatoconfig(self.printData)
+            self._pagesetupdatatoconfig(pageDialog.GetPageSetupData())
+        pageDialog.Destroy()
+
+    def PreviewText(self, htmltext, basepath="", scale=1.0):
+        printout1=self._getprintout(htmltext, basepath, scale)
+        printout2=self._getprintout(htmltext, basepath, scale)
+        preview=wx.PrintPreview(printout1, printout2, self.printData)
+        if not preview.Ok():
+            print "preview problem"
+            assert False, "preview problem"
+            return
+        frame=wx.PreviewFrame(preview, self.parent, "Print Preview")
+        frame.Initialize()
+        # frame.SetPosition(self.parent.GetPosition())
+        # frame.SetSize(self.parent.GetSize())
+        frame.Show(True)
+
+    def PrintText(self, htmltext, basepath="", scale=1.0):
+        pdd=wx.PrintDialogData()
+        pdd.SetPrintData(self.printData)
+        printer=wx.Printer(pdd)
+        printout=self._getprintout(htmltext, basepath, scale)
+        if printer.Print(self.parent, printout):
+            self.printData=printer.GetPrintDialogData().GetPrintData()
+            self._printdatatoconfig(self.printData)
+        printout.Destroy()
+
+    def _getprintout(self, htmltext, basepath, scale):
+        print "scale is",scale," margins are", self.margins
+        printout=wx.html.HtmlPrintout()
+        basesizes=[7,8,10,12,16,22,30]
+        printout.SetFonts("", "", [int(sz*scale) for sz in basesizes])
+        printout.SetMargins(*self.margins)
+        printout.SetHtmlText(htmltext, basepath)
+        return printout
+
+    def _printdatatoconfig(self, pd):
+        if self.config is None: return
+        c=self.config
+        cstr=self.configstr
+
+        for key,func in [ ("collate", pd.GetCollate),
+                          ("colour", pd.GetColour),
+                          ("duplex", pd.GetDuplex),
+                          ("nocopies", pd.GetNoCopies),
+                          ("orientation", pd.GetOrientation),
+                          ("paperid", pd.GetPaperId),
+                          ]:
+            c.WriteInt(cstr+"/"+key, func())
+        c.Write(cstr+"/printer", pd.GetPrinterName())
+        c.Flush()
+        
+    def _configtoprintdata(self, pd):
+        if self.config is None: return
+        c=self.config
+        cstr=self.configstr
+        for key,func in [ ("collate", pd.SetCollate),
+                          ("colour", pd.SetColour),
+                          ("duplex", pd.SetDuplex),
+                          ("nocopies", pd.SetNoCopies),
+                          ("orientation", pd.SetOrientation),
+                          ("paperid", pd.SetPaperId),
+                          ]:
+            if c.HasEntry(cstr+"/"+key): func(c.ReadInt(cstr+"/"+key))
+        # special case - set paper to letter if not explicitly set (wx defaults to A4)
+        if not c.HasEntry(cstr+"/paperid"):
+            pd.SetPaperId(wx.PAPER_LETTER)
+        # printer name
+        pd.SetPrinterName(c.Read(cstr+"/printer", ""))
+
+    def _configtopagesetupdata(self, psdd=None):
+        v=self.config.Read(self.configstr+"/margins", "")
+        if len(v):
+            l=[int(x) for x in v.split(',')]
+        else:
+            l=[15,15,15,15]
+        if psdd is not None:
+            psdd.SetMarginTopLeft( (l[0], l[1]) )
+            psdd.SetMarginBottomRight( (l[2], l[3]) )
+        self.margins=l
+
+    def _pagesetupdatatoconfig(self, psdd):
+        tl=psdd.GetMarginTopLeft()
+        br=psdd.GetMarginBottomRight()
+        v="%d,%d,%d,%d" % (tl[0], tl[1], br[0], br[1])
+        self.config.Write(self.configstr+"/margins", v)
+        self.margins=[tl[0],tl[1],br[0],br[1]]
+        
+
 if __name__=='__main__':
     src="""
 <HTML>
@@ -312,3 +446,47 @@ if __name__=='__main__':
     f.Show(True)
     h.SetPage(tp.flatten())
     app.MainLoop()
+    sys.exit(0)
+
+# a test of the easy printing
+if __name__=='__main__':
+    import sys
+
+    def OnSlider(evt):
+        global scale
+        scale=szlist[evt.GetPosition()]
+
+    f=open(sys.argv[1], "rt")
+    html=f.read()
+    f.close()
+
+    app=wx.PySimpleApp()
+
+    f=wx.Frame(None, -1, "Print Test")
+    butsetup=wx.Button(f, wx.NewId(), "Setup")
+    butpreview=wx.Button(f, wx.NewId(), "Preview")
+    butprint=wx.Button(f, wx.NewId(), "Print")
+
+    slider=wx.Slider(f, wx.NewId(), 5, 0, 10, style=wx.SL_HORIZONTAL)
+
+    szlist=[0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4]
+
+    scale=1.0
+
+    bs=wx.BoxSizer(wx.HORIZONTAL)
+    bs.Add(slider, 1, wx.EXPAND|wx.ALL, 5)
+    for i in (butsetup,butpreview, butprint):
+        bs.Add(i, 0, wx.EXPAND|wx.ALL, 5)
+    f.SetSizer(bs)
+    bs.Fit(f)
+
+    hep=HtmlEasyPrinting(f, None, None)
+
+    wx.EVT_BUTTON(f, butsetup.GetId(), lambda _: hep.PrinterSetup())
+    wx.EVT_BUTTON(f, butpreview.GetId(), lambda _: hep.PreviewText(html, scale=scale))
+    wx.EVT_BUTTON(f, butprint.GetId(), lambda _: hep.PrintText(html, scale=scale))
+    wx.EVT_COMMAND_SCROLL(f, slider.GetId(), OnSlider)
+
+    f.Show(True)
+    app.MainLoop()
+    
