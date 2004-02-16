5774db5492546b9d8c91b31d5498b6489e963d3d
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-02-16 03:37:35 +0000

    start 0.7 test5 code
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@933 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/version.py
+++ src/version.py
@@ -8,7 +8,7 @@ import time
 name="BitPim"
 version="0.7"
 release=0  # when rereleases of the same version happen, this gets incremented
-testver=4  # value of zero is non-test build
+testver=5  # value of zero is non-test build
 extrainfo="" # More gunk should it be test version
 contact="The BitPim home page is at http://bitpim.sf.net.  You can post any " \
          "questions or feedback to the mailing list detailed on that page." # where users are sent to contact with feedback
