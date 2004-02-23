c9258d38fd9bd1f87ec412acb0b244976b71b978
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-02-23 09:29:41 +0000

    - make bitfling a module - client side stub
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@962 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/bitfling/__init__.py
@@ -0,0 +1,2 @@
+# $Id$
+# This file does nothing except establish this directory as a module
---
+++ src/bitfling/client.py
@@ -0,0 +1,17 @@
+### BITPIM
+###
+### Copyright (C) 2004 Roger Binns <rogerb@rogerbinns.com>
+###
+### This software is under the Artistic license.
+### Please see the accompanying LICENSE file
+###
+### $Id$
+
+"""Code if you want to be a client of BitFling"""
+
+# Currently this file does nothing except some imports.  That
+# is to help test the installer generators etc
+
+import xmlrpcstuff
+
+print "BitFling client imported"
