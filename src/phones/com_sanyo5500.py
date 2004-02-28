9c2c8ad3c83796693535ec3a2a1065eecab72646
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-02-28 12:53:00 +0000

    Add/update local sendpbcommand and getsanyobuffer so that getphonebook can be removed
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@987 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_sanyo5500.py
+++ src/phones/com_sanyo5500.py
@@ -46,6 +46,11 @@ class Phone(com_sanyo.Phone):
         com_sanyo.Phone.__init__(self, logtarget, commport)
         self.mode=self.MODENONE
 
+    def sendpbcommand(self, request, responseclass, callsetmode=True, writemode=False, numsendretry=2):
+         
+        res=com_sanyo.Phone.sendpbcommand(self, request, responseclass, callsetmode=callsetmode, writemode=writemode, numsendretry=numsendretry)
+        return res
+ 
 #
 # Almost identical to com_sanyo.py version.
 #
@@ -55,11 +60,14 @@ class Phone(com_sanyo.Phone):
         data=cStringIO.StringIO()
         bufp=0
         command=startcommand
+        req=self.protocolclass.bufferpartrequest()
+        req.header.command=command
+        if command==0xd7:
+            req.header.packettype=0x0c
+            
         for offset in range(0, buffersize, 1024):
 #            self.progress(data.tell(), buffersize, desc)
-            req=self.protocolclass.bufferpartrequest()
-            req.header.command=command
-            res=self.sendpbcommand(req, self.protocolclass.bufferpartresponse,numsendretry=2);
+            res=self.sendpbcommand(req, self.protocolclass.bufferpartresponse);
             data.write(res.data)
             command+=1
 
@@ -71,79 +79,9 @@ class Phone(com_sanyo.Phone):
         assert buffersize==len(data)
         return data
 
-#
-# Almost identical to com_sanyo.py version.
-#
-    def getphonebook(self,result):
-        pbook={}
-        # Get Sort buffer so we know which of the 300 phone book slots
-        # are in use.
+        return res
 
-        sortstuff=self.protocolclass.pbsortbuffer()
-        buf=prototypes.buffer(self.getsanyobuffer(sortstuff.startcommand, sortstuff.bufsize, "sort buffer"))
-        sortstuff.readfrombuffer(buf)
-
-        # ringpic=self.protocolclass.ringerpicbuffer()
-        # buf=prototypes.buffer(self.getsanyobuffer(ringpic.startcommand, ringpic.bufsize, "ringer/picture assignments"))
-        req=self.protocolclass.ringerpicbufferrequest()
-        res=self.sendpbcommand(req, self.protocolclass.ringerpicbufferresponse,numsendretry=2)
-        ringpic = res.buffer
-
-        speedslot=[]
-        speedtype=[]
-        for i in range(self.protocolclass._NUMSPEEDDIALS):
-            speedslot.append(sortstuff.speeddialindex[i].pbslotandtype & 0xfff)
-            numtype=(sortstuff.speeddialindex[i].pbslotandtype>>12)-1
-            if(numtype >= 0 and numtype <= len(numbertypetab)):
-                speedtype.append(numbertypetab[numtype])
-            else:
-                speedtype.append("")
-
-        numentries=sortstuff.slotsused
-        self.log("There are %d entries" % (numentries,))
-        
-        count = 0
-        for i in range(0, self.protocolclass._NUMPBSLOTS):
-            if sortstuff.usedflags[i].used:
-                ### Read current entry
-                req=self.protocolclass.phonebookslotrequest()
-                req.slot = i
-                res=self.sendpbcommand(req, self.protocolclass.phonebookslotresponse,numsendretry=2)
-                self.log("Read entry "+`i`+" - "+res.entry.name)
-
-                entry=self.extractphonebookentry(res.entry, result)
-                # Speed dials
-                for j in range(len(speedslot)):
-                    if(speedslot[j]==req.slot):
-                        for k in range(len(entry['numbers'])):
-                            if(entry['numbers'][k]['type']==speedtype[j]):
-                                entry['numbers'][k]['speeddial']=j+2
-                                break
-
-                # ringtones
-                if ringpic.ringtones[i].ringtone>0:
-                    try:
-                        tone=result['ringtone-index'][ringpic.ringtones[i].ringtone]['name']
-                    except:
-                        tone=self.serialsname+"Index_"+`ringpic.ringtones[i].ringtone`
-                    entry['ringtones']=[{'ringtone': tone, 'use': 'call'}]
-
-                # wallpapers
-                if ringpic.wallpapers[i].wallpaper>0:
-                    try:
-                        paper=result['wallpaper-index'][ringpic.wallpapers[i].wallpaper]['name']
-                    except:
-                        paper=self.serialsname+"Index_"+`ringpic.wallpapers[i].wallpaper`
-                    entry['wallpapers']=[{'wallpaper': paper, 'use': 'call'}]
-                    
-                pbook[count]=entry 
-                self.progress(count, numentries, res.entry.name)
-                count+=1
-        
-        self.progress(numentries, numentries, "Phone book read completed")
-        result['phonebook']=pbook
-        return pbook
-
+    
 class Profile(com_sanyo.Profile):
 
     protocolclass=p_sanyo5500
