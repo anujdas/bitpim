de848d97eab078122f9846d0e35ddd1b21768ab2
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-03-24 05:18:16 +0000

    Add 7300, phonebook only
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1065 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/phones/com_sanyo7300.py
@@ -0,0 +1,48 @@
+### BITPIM
+###
+### Copyright (C) 2003-2004 Stephen Wood <sawecw@users.sf.net>
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the BitPim license as detailed in the LICENSE file.
+###
+### $Id$
+
+"""Talk to the Sanyo SCP-7300 (RL2000) cell phone"""
+
+# my modules
+import common
+import p_sanyo7300
+import com_brew
+import com_phone
+import com_sanyo
+import prototypes
+
+
+class Phone(com_sanyo.Phone):
+    "Talk to the Sanyo SCP-7300 cell phone"
+
+    desc="SCP-7300"
+
+    protocolclass=p_sanyo7300
+    serialsname='scp7300'
+
+    builtinringtones=( 'None', 'Vibrate')
+                      
+    calendar_defaultringtone=0
+
+    def __init__(self, logtarget, commport):
+        com_sanyo.Phone.__init__(self, logtarget, commport)
+        self.mode=self.MODENONE
+
+class Profile(com_sanyo.Profile):
+
+    protocolclass=p_sanyo7300
+    serialsname='scp7300'
+
+    _supportedsyncs=(
+        ('phonebook', 'read', None),  # all phonebook reading
+        ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
+    )
+
+    def __init__(self):
+        com_sanyo.Profile.__init__(self)
