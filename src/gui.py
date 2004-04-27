7a273a69c9577bb5fbf974553d424b05427088f4
n9yty <n9yty@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-04-27 02:39:33 +0000

    Temporary patch to startup logic for Mac only. Don't open the setup dialog on first launch, just set defaults; the user will have to manually go into settings.
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1182 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/gui.py
+++ src/gui.py
@@ -495,11 +495,22 @@ class MainWindow(wx.Frame):
 
         ### Is config set?
         self.configdlg=guiwidgets.ConfigDialog(self, self)
-        if self.configdlg.needconfig():
-            self.CloseSplashScreen()
-            if self.configdlg.ShowModal()!=wx.ID_OK:
-                self.OnExit()
-                return
+        #
+        # N9YTY: TEMPORARY WORKAROUND!
+        #
+        # On the Mac, calling the dialog at this point is unresponsive.
+        # So we just set defaults and let things go on... The user will
+        # have to be smart enough to open the settings dialog themselves
+        # for now. Maybe set a global variable that settings aren't there
+        # and fire a timer event after the main window has opened??
+        if guihelper.IsMac():
+            self.configdlg.setdefaults()
+        else:
+            if self.configdlg.needconfig():
+                self.CloseSplashScreen()
+                if self.configdlg.ShowModal()!=wx.ID_OK:
+                    self.OnExit()
+                    return
         self.configdlg.updatevariables()
         
         ### notebook
