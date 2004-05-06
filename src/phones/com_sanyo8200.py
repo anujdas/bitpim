52865de07f7da3b38b7b15b0a3b4911938aca3b3
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-05-06 15:45:53 +0000

    Draft of code for PM8200.
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1240 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/phones/com_sanyo8200.py
@@ -0,0 +1,61 @@
+### BITPIM
+###
+### Copyright (C) 2004 Stephen Wood <sawecw@users.sf.net>
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the BitPim license as detailed in the LICENSE file.
+###
+### $Id$
+
+"""Talk to the Sanyo SCP-8200 cell phone"""
+
+# my modules
+import common
+import p_sanyo8200
+import com_brew
+import com_phone
+import com_sanyo
+import com_sanyomedia
+import com_sanyonewer
+import prototypes
+
+numbertypetab=( 'cell', 'home', 'office', 'pager',
+                    'fax', 'data', 'none' )
+
+class Phone(com_sanyonewer.Phone):
+    "Talk to the Sanyo PM8200 cell phone"
+
+    desc="PM8200"
+
+    FIRST_MEDIA_DIRECTORY=1
+    LAST_MEDIA_DIRECTORY=3
+
+    imagelocations=(
+        # offset, directory #, indexflag, type, maximumentries
+        )    
+
+    protocolclass=p_sanyo8200
+    serialsname='pm8200'
+
+    calendar_defaultringtone=0
+
+    def __init__(self, logtarget, commport):
+        com_sanyonewer.Phone.__init__(self, logtarget, commport)
+        self.mode=self.MODENONE
+
+class Profile(com_sanyonewer.Profile):
+
+    protocolclass=p_sanyo8200
+    serialsname='pm8200'
+
+    _supportedsyncs=(
+        ('phonebook', 'read', None),  # all phonebook reading
+        ('calendar', 'read', None),   # all calendar reading
+        ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
+        ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
+        ('wallpaper', 'read', None),  # all wallpaper reading
+        ('ringtone', 'read', None),  # all ringtone reading
+    )
+
+    def __init__(self):
+        com_sanyonewer.Profile.__init__(self)
