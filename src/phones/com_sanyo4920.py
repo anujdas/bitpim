e7b00e227b6f82fd69e06037f19050d4eb1a75df
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-05-26 23:43:21 +0000

    Place holder for unreleased phone
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1300 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/phones/com_sanyo4920.py
@@ -0,0 +1,66 @@
+### BITPIM
+###
+### Copyright (C) 2004 Stephen Wood <sawecw@users.sf.net>
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the BitPim license as detailed in the LICENSE file.
+###
+### $Id$
+
+"""Talk to the Sanyo RL-4920 cell phone"""
+
+# Phone has 50 minute digital voice memo recorder.
+# Sanyo description says recordings can be downloaded to PC using
+# data cable.
+
+# my modules
+import common
+import p_sanyo4920
+import com_brew
+import com_phone
+import com_sanyo
+import com_sanyomedia
+import com_sanyonewer
+import prototypes
+
+# Uncomment if it entry turns out to be ordered like PM-8200
+#numbertypetab=( 'cell', 'home', 'office', 'pager',
+#                    'fax', 'data', 'none' )
+
+class Phone(com_sanyonewer.Phone):
+    "Talk to the Sanyo RL-4920 cell phone"
+
+    desc="SCP-4920"
+
+    FIRST_MEDIA_DIRECTORY=2
+    LAST_MEDIA_DIRECTORY=3
+
+    imagelocations=(
+        # offset, directory #, indexflag, type, maximumentries
+        )    
+
+    protocolclass=p_sanyo4920
+    serialsname='rl4920'
+
+    calendar_defaultringtone=4
+
+    def __init__(self, logtarget, commport):
+        com_sanyonewer.Phone.__init__(self, logtarget, commport)
+        self.mode=self.MODENONE
+
+class Profile(com_sanyonewer.Profile):
+
+    protocolclass=p_sanyo4920
+    serialsname='rl4920'
+
+    _supportedsyncs=(
+        ('phonebook', 'read', None),  # all phonebook reading
+        ('calendar', 'read', None),   # all calendar reading
+        ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
+        ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
+        ('wallpaper', 'read', None),  # all wallpaper reading
+        ('ringtone', 'read', None),   # all ringtone reading
+    )
+
+    def __init__(self):
+        com_sanyonewer.Profile.__init__(self)
