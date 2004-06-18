2664d26542895fca6200ea6a00393f3d534468c7
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-06-18 07:19:43 +0000

    - more work on understanding vcards - more samples from apple, mozilla and palm desktop
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1348 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/vcard.py
+++ src/vcard.py
@@ -75,6 +75,7 @@ class VFile:
             # (see 5.8.2 in rfc 2425)
             # unencode anything that needs it
             if not i.startswith("ENCODING=") and not i=="QUOTED-PRINTABLE": # evolution doesn't bother with "ENCODING="
+                # ::TODO:: deal with backslashes, being especially careful with ones quoting semicolons
                 newitems.append(i)
                 continue
             try:
@@ -90,6 +91,8 @@ class VFile:
                 raise VFileException("Exception %s while processing encoding %s on data '%s'" % (str(e), i, line))
         # ::TODO:: repeat above shenanigans looking for a VALUE= thingy and
         # convert line as in 5.8.4 of rfc 2425
+        if len(newitems)==0:
+            raise VFileException("Line contains no property: %s" % (line,))
         if newitems==["BEGIN"] or newitems==["END"]:
             line=line.upper()
         return newitems,line
@@ -153,10 +156,19 @@ class VCard:
     "A single vcard"
 
     def __init__(self, lines):
-        self._version=(2,0)
+        self._version=(2,0)  # which version of the vcard spec the card conforms to
+        self._origin=None    # which program exported the vcard
+        self._data={}
         self.lines=[]
         # extract version field
         for f,v in lines:
+            assert len(f)
+            if f==["X-EVOLUTION-FILE-AS"]: # all evolution cards have this
+                self._origin="evolution"
+            if f[0].startswith("ITEM") and (f[0].endswith(".X-ABADR") or f[0].endswith(".X-ABLABEL")):
+                self._origin="apple"
+            if len(v) and v[0].find(">!$_") > v[0].find("_$!<") >=0:
+                self.origin="apple"
             if f==["VERSION"]:
                 ver=v.split(".")
                 try:
@@ -165,13 +177,65 @@ class VCard:
                     raise VFileException(v+" is not a valid vcard version")
                 self._version=ver
                 continue
+            # convert {home,work}.{tel,label} to {tel,label};{home,work}
+            # this probably dates from *very* early vcards
+            if f[0]=="HOME.TEL": f[0:1]=["TEL", "HOME"]
+            elif f[0]=="HOME.LABEL": f[0:1]=["LABEL", "HOME"]
+            elif f[0]=="WORK.TEL": f[0:1]=["TEL", "WORK"]
+            elif f[0]=="WORK.LABEL": f[0:1]=["LABEL", "WORK"]
             self.lines.append( (f,v) )
+        self._parse(self.lines, self._data)
+
+    def _getfieldname(self, name, dict):
+        """Returns the fieldname to use in the dict.
+
+        For example, if name is "email" and there is no "email" field
+        in dict, then "email" is returned.  If there is already an "email"
+        field then "email2" is returned, etc"""
+        if name not in dict:
+            return name
+        for i in range(2,999999):
+            if name+`i` not in dict:
+                return name+`i`
+
+    def _parse(self, lines, result):
+        for field,value in lines:
+            if '.' in field[0]:
+                f=field[0][field[0].find('.')+1:]
+            else: f=field[0]
+            t=f.replace("-", "_")
+            func=getattr(self, "_field_"+t, self._default_field)
+            func(field, value, result)
+
+    def _field_FN(self, field, value, result):
+        result[self._getfieldname("name", result)]=self.unquote(value)
+
+    def _default_field(self, field, value, result):
+        if field[0].startswith("X-"):
+            print "ignoring custom field",field
+            return
+        print "no idea what do with"
+        print "field",field
+        print "value",value
+
+    def unquote(self, value):
+        # ::TODO:: do this properly
+        value=value.replace(r"\;", ";")
+        value=value.replace(r"\n", "\n")
+        return value
+
 
     def version(self):
+        "Best guess as to vcard version"
         return self._version
+
+    def origin(self):
+        "Best guess as to what program wrote the vcard"
+        return self._origin
 
     def __repr__(self):
         str="Version: %s\n" % (`self.version()`)
+        str+="Origin: %s\n" % (`self.origin()`)
         str+=`self.lines`
         return str
 
