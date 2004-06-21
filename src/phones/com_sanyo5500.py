d23555a5504e9dde7eb40595b773e327c50ba6da
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-06-21 04:17:08 +0000

    Use default wallpaper size and supported syncs
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1360 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_sanyo5500.py
+++ src/phones/com_sanyo5500.py
@@ -54,17 +54,5 @@ class Profile(com_sanyonewer.Profile):
     protocolclass=p_sanyo5500
     serialsname='scp5500'
 
-    WALLPAPER_WIDTH=132
-    WALLPAPER_HEIGHT=176
-    
-    _supportedsyncs=(
-        ('phonebook', 'read', None),  # all phonebook reading
-        ('calendar', 'read', None),   # all calendar reading
-        ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
-        ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
-        ('wallpaper', 'read', None),  # all wallpaper reading
-        ('ringtone', 'read', None),   # all ringtone reading
-    )
-
     def __init__(self):
         com_sanyonewer.Profile.__init__(self)
