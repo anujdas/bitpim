37c244e081a496684d347aed83b8a4e802fb8e92
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-07-02 23:38:24 +0000

    Place holder as we await information about Canadian Bell 8100.
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1399 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/phones/com_sanyo8100_bell.py
@@ -0,0 +1,74 @@
+### BITPIM
+###
+### Copyright (C) 2003-2004 Stephen Wood <sawecw@users.sf.net>
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the BitPim license as detailed in the LICENSE file.
+###
+### $Id$
+
+"""Talk to the Sanyo SCP-8100 Bell Mobility (Canada) cell phone"""
+
+# my modules
+import common
+import p_sanyo8100_bell
+import com_brew
+import com_phone
+import com_sanyo
+import com_sanyomedia
+import com_sanyonewer
+import prototypes
+
+import os
+
+class Phone(com_sanyomedia.SanyoMedia,com_sanyo.Phone):
+    "Talk to the Sanyo SCP-8100 Bell Mobility (Canada) cell phone"
+
+    desc="SCP-8100-Bell"
+
+    FIRST_MEDIA_DIRECTORY=1
+    LAST_MEDIA_DIRECTORY=3
+
+    protocolclass=p_sanyo8100_bell
+    serialsname='scp8100bell'
+    
+# Need to check these from Bell
+
+    builtinringtones=( 'None', 'Vibrate', 'Ringer & Voice', '', '', '', '', '', '', 
+                       'Tone 1', 'Tone 2', 'Tone 3', 'Tone 4', 'Tone 5',
+                       'Tone 6', 'Tone 7', 'Tone 8', '', '', '', '', '',
+                       '', '', '', '', '', '', '',
+                       'Tschaik.Swanlake', 'Satie Gymnop.#1',
+                       'Bach Air on the G', 'Beethoven Sym.5', 'Greensleeves',
+                       'Johnny Comes..', 'Foster Ky. Home', 'Asian Jingle',
+                       'Disco' )
+
+    calendar_defaultringtone=4
+
+    def __init__(self, logtarget, commport):
+        com_sanyo.Phone.__init__(self, logtarget, commport)
+        com_sanyomedia.SanyoMedia.__init__(self)
+        self.mode=self.MODENONE
+
+class Profile(com_sanyo.Profile):
+
+    protocolclass=p_sanyo8100_bell
+    serialsname='scp8100bell'
+
+    WALLPAPER_WIDTH=120
+    WALLPAPER_HEIGHT=144
+    OVERSIZE_PERCENTAGE=100
+
+    _supportedsyncs=(
+        ('phonebook', 'read', None),  # all phonebook reading
+        ('calendar', 'read', None),   # all calendar reading
+#        ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
+#        ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
+        ('wallpaper', 'write', 'OVERWRITE'),
+        ('ringtone', 'write', 'OVERWRITE'),
+        ('wallpaper', 'read', None),  # all wallpaper reading
+        ('ringtone', 'read', None),   # all ringtone reading
+    )
+
+    def __init__(self):
+        com_sanyo.Profile.__init__(self)
