84eebfaed48370eb4b7979bf7ac5f72fbeaf0d9e
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-07-10 08:27:11 +0000

    changed several files from .bat to .sh
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1429 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ help/makehelp.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# This should only run on Windows since we also need to build CHM files
+# although in theory it could run against the Linux and Mac versions
+# of helpblocks
+
+# These are what all the control files are
+#
+# HHP   project file (MS Html Help workshop)
+# HHC   table of contents file
+# HHK   index
+# WXH   project file (HelpBlocks) XML
+
+# windows fudge
+PATH=/usr/bin:$PATH
+
+PYTHON=python
+HBDIR="/c/program files/helpblocks"
+
+# version info for helpblocks pre-processor
+$PYTHON version.py > help/version.h
+
+# update web tree of docs
+cd help
+$PYTHON contentsme.py bitpim.hhc webhelp/navtree.html
+
+# remove old files
+rm *.htm bitpim.chm bitpim.htb ../resources/bitpim.chm ../resources/bitpim.htb
+
+# Run helpblocks
+# hb errors unless the cwd is its install dir
+oldpwd="`pwd`"
+
+cd "$HBDIR"
+./helpblocks.exe --rebuild --chm --wxhtml "$oldpwd/bitpim.wxh"
+cd "$oldpwd"
+
+# generate various ids
+$PYTHON genids.py bitpim_alias.h ../helpids.py
+cp bitpim.htb bitpim.chm ../resources
+
+# did anyone forget to rename files?
+if [ `grep doc- bitpim.hhp | wc -l` -gt 0 ]
+then
+     echo "You forgot to rename some files"
+     grep doc- bitpim.hhp
+     exit 1
+fi
+
+cd ..
+
+# copy into website
+if [ -d ../bpweb/site/CVS ]
+then
+    ver=`$PYTHON version.py --majorminor`
+    echo "Copying $ver help into web site tree"
+    # webhelp="`pwd`/../bpweb/site/help/$ver" # what i really want
+    webhelp="`pwd`/../bpweb/site/testhelp"
+    rm -rf "$webhelp"
+    mkdir -p "$webhelp"
+    cd help
+    cp webhelp/*.{html,js,css} "$webhelp"
+    mkdir "$webhelp/img"
+    cp webhelp/img/*.gif "$webhelp/img"
+    unzip bitpim.htb -d "$webhelp"
+fi
