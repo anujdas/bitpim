6c64f4c215bdd224e0e90062dec252f35bf9f47e
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-08-06 05:37:47 +0000

    0.7.16 starts
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1468 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/version.py
+++ src/version.py
@@ -12,7 +12,7 @@
 import time
 
 name="BitPim"
-version="0.7.15"
+version="0.7.16"
 vendor="official"
 release=0  # when rereleases of the same version happen, this gets incremented
 testver=0  # value of zero is non-test build
