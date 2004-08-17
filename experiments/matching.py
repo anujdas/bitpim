d239e137a41639eb5f7add82b9f14c1f24b545ea
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-08-17 21:19:20 +0000

    attempt to use Levenshtein module to see how well that does
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1511 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- experiments/matching.py
+++ experiments/matching.py
@@ -3,15 +3,26 @@
 # Do string matching giving a confidence score.
 
 import difflib
+import Levenshtein
 
 # according to doc, should call set_seq2 once and then keep changing
 # seq1
 
-def match(s1, s2):
+def dlmatch(s1, s2):
+    "how well does s2 match s1?"
     s=difflib.SequenceMatcher()
     s.set_seq2(s1)
     s.set_seq1(s2)
     return int(s.ratio()*100)
+
+def lvmatch(s1, s2):
+    "how well does s2 match s1?"
+    return int(Levenshtein.jaro_winkler(s1,s2)*100)
+
+def srmatch(s1, s2):
+    s3=s2.split()
+    s3.reverse()
+    return max(lvmatch(s1,s2), lvmatch(s1," ".join(s3)))
 
 inp="""
 John Smith
@@ -43,9 +54,9 @@ for one in inp:
     print
     l=[]
     for two in inp:
-        l.append( (match(one, two), two) )
+        l.append( (srmatch(one, two), lvmatch(one, two), dlmatch(one,two), two) )
     l.sort()
     l.reverse()
-    for score,name in l:
-        print "%3d %s" % (score,name)
+    for srscore, lvscore,dlscore,name in l:
+        print "%3d %3d %3d %s" % (srscore, lvscore,dlscore,name)
     print
