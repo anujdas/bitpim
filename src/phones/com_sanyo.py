5e5a2fd4ab90803c764437e19a9317ad4903f88f
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-10-08 01:29:22 +0000

    Allow upper case letters in files written to phone.  Other allowed characters to be added as they are verified
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1642 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_sanyo.py
+++ src/phones/com_sanyo.py
@@ -1171,11 +1171,11 @@ class Profile(com_phone.Profile):
     OVERSIZE_PERCENTAGE=100
     
     MAX_WALLPAPER_BASENAME_LENGTH=19
-    WALLPAPER_FILENAME_CHARS="abcdefghijklmnopqrstuvwxyz0123456789 ."
+    WALLPAPER_FILENAME_CHARS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ."
     WALLPAPER_CONVERT_FORMAT="png"
     
     MAX_RINGTONE_BASENAME_LENGTH=19
-    RINGTONE_FILENAME_CHARS="abcdefghijklmnopqrstuvwxyz0123456789 ."
+    RINGTONE_FILENAME_CHARS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ."
 
     # which usb ids correspond to us
     usbids=( ( 0x0474, 0x0701, 1),  # VID=Sanyo, PID=4900 internal USB interface
