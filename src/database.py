a17d406ca26eab25abe8eedc2bad5f165d2f5f90
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-11-26 05:08:10 +0000

    started work on trying to create and auto-expand table schemas
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1781 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/database.py
+++ src/database.py
@@ -10,17 +10,32 @@
 """Interface to the database"""
 
 import os
+import threading
 
 import sqlite
 
 
 def ExclusiveWrapper(method):
-    def _transactionwrapper(self, *args, **kwargs):
-        self.cursor.execute("BEGIN EXCLUSIVE TRANSACTION")
+    """Wrap a method so that it has an exclusive lock on the database
+    (noone else can read or write) until it has finished"""
+
+    # pysqlite currently tries to issue its own BEGINs and ENDs which breaks things
+    
+    def _transactionwrapper(*args, **kwargs):
+        exlock=getattr(args[0], "exlock")
+        exlock.acquire()
+        cursor=getattr(args[0], "cursor")
+        # RLock doesn't expose the count nicely so we have to dig inside it
+        if exlock._RLock__count==1:
+            # cursor.execute("BEGIN EXCLUSIVE TRANSACTION")
+            pass
         try:
-            return method(self, *args, **kwargs)
+            return method(*args, **kwargs)
         finally:
-            self.cursor.execute("COMMIT TRANSACTION")
+            if exlock._RLock__count==1:
+                # cursor.execute("COMMIT TRANSACTION")
+                pass
+            exlock.release()
 
     return _transactionwrapper
 
@@ -76,8 +91,110 @@ def sqltokens(sql):
             yield res
 
 
+def sqlquote(s):
+    "returns an sqlite quoted string (the return value will begin and end with single quotes)"
+    return "'"+s.replace("'", "''")+"'"
+
 class Database:
 
     def __init__(self, directory, filename):
         self.connection=sqlite.connect(os.path.join(directory, filename))
-        self.cursor=self.connection.cursor
+        self.cursor=self.connection.cursor()
+        self.exlock=threading.RLock()
+
+    def sql(self, statement, *params):
+        "Execute statement and return a generator of the results"
+        print "SQL:",statement
+        self.cursor.execute(statement,params)
+        return self.cursor
+
+    def doestableexist(self, tablename):
+        return bool(self.sql("select count(*) from sqlite_master where type='table' and name=%s" % (sqlquote(tablename),)).next()[0])
+
+    def savemajordict(self, tablename, dict):
+        """This is the entrypoint for saving a first level dictionary
+        such as the phonebook or calendar.
+
+        @param tablename: name of the table to use
+        @param dict: The dictionary of values.  The key is ignored, and
+                     the value of each key is treated as a record
+        """
+        
+        # make sure the table exists first
+        if not self.doestableexist(tablename):
+            # create table and include meta-fields
+            self.sql("create table %s (__rowid__ integer primary key, __timestamp__ TIMESTAMP, __deleted__ integer, __uid__ varchar)" % (sqlquote(tablename),))
+        # examine the keys in dict
+        dk=[]
+        for k in dict.keys():
+            for kk in dict[k]:
+                if kk not in dk:
+                    dk.append(kk)
+        # verify that they don't start with __
+        assert len([k for k in dk if k.startswith("__")])==0
+        # get database keys
+        dbkeys=[name for _, name, _, _, _, _ in self.sql("pragma table_info(%s)" % (sqlquote(tablename),))]
+        # are any missing?
+        missing=[k for k in dk if k not in dbkeys]
+        if len(missing):
+            creates=[]
+            # for each missing key, we have to work out if the value
+            # is a list type (which we indirect to another table)
+            for m in missing:
+                islist=None
+                isnotlist=None
+                for r in dict.keys():
+                    record=dict[r]
+                    v=record.get(m,None)
+                    if v is None:
+                        continue
+                    if isinstance(v, list):
+                        islist=record
+                    else:
+                        isnotlist=record
+                if islist is None and isnotlist is None:
+                    # they have the key but no record has any values, so we ignore it
+                    del dk[dk.index(m)]
+                    continue
+                if islist is not None and isnotlist is not None:
+                    # can't have it both ways
+                    raise ValueError("key %s for table %s has some values as list as some as not. eg LIST: %s, NOTLIST: %s" % (m,tablename,`islist`,`isnotlist`))
+                if isnotlist is not None:
+                    creates.append( (m, "value") )
+                    continue
+                if islist is not None:
+                    creates.append( (m, "indirect") )
+                    continue
+            if len(creates):
+                # alter the table to add the new fields - sqlite doesn't support alter table so we do it manually
+                dbtkeys=[(name,type) for _, name, type, _, _, _ in self.sql("pragma table_info(%s)" % (sqlquote(tablename),))]
+                cmd=["create", "temporary", "table", sqlquote("backup_"+tablename), "("]
+                for n,t in dbtkeys:
+                    if cmd[-1]!="(":
+                        cmd.append(",")
+                    cmd.append(sqlquote(n))
+                    cmd.append(t)
+                cmd.append(")")
+                self.sql(" ".join(cmd))
+                # copy the values into the temporary table
+                self.sql("insert into %s select * from %s" % (sqlquote("backup_"+tablename), sqlquote(tablename)))
+                # drop the source table
+                self.sql("drop table %s" % (sqlquote(tablename),))
+                # recreate the source table with new columns
+                del cmd[1] # remove temporary
+                cmd[2]=sqlquote(tablename) # change tablename
+                self.sql(" ".join(cmd))
+                # put values back in
+                self.sql("insert into %s select * from %s" % (sqlquote(tablename), sqlquote("backup_"+tablename)))
+                
+    savemajordict=ExclusiveWrapper(savemajordict)
+
+
+if __name__=='__main__':
+    db=Database(".", "testdb")
+
+    # use the phonebook out of the examples directory
+    execfile("examples/phonebook-index.idx")
+
+    db.savemajordict("phonebook", phonebook)
+        
