bf9aab0cd23629844609ec7fd8494dc462a8cfb6
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-11-30 07:27:01 +0000

    it works except for a few small bugs!
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1788 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/database.py
+++ src/database.py
@@ -98,6 +98,12 @@ def sqlquote(s):
     "returns an sqlite quoted string (the return value will begin and end with single quotes)"
     return "'"+s.replace("'", "''")+"'"
 
+def idquote(s):
+    """returns an sqlite quoted identified (eg for when a column name is also an SQL keyword
+
+    The value returned is quoted in square brackets"""
+    return '['+s+']'
+
 class Database:
 
     def __init__(self, directory, filename):
@@ -113,29 +119,43 @@ class Database:
         self.cursor.execute(statement,params)
         return self.cursor
 
+    def sqlmany(self, statement, params):
+        "Like cursor.executemany but it actually works"
+        # get another cursor
+        cursor=self.connection.cursor()
+        for p in params:
+            print "SQLM:",statement,p
+            cursor.execute(statement, p)
+            for res in cursor:
+                yield res
+
     def doestableexist(self, tablename):
         return bool(self.sql("select count(*) from sqlite_master where type='table' and name=%s" % (sqlquote(tablename),)).next()[0])
 
     def getcolumns(self, tablename):
-        l=[x for x in self.sql("pragma table_info("+sqlquote(tablename)+")")]
+        l=[x for x in self.sql("pragma table_info("+idquote(tablename)+")")]
         for colnum,name,type, _, default, primarykey in l:
             if primarykey:
                 type+=" primary key"
-            yield name,type
+            yield colnum,name,type
 
     def savemajordict(self, tablename, dict):
         """This is the entrypoint for saving a first level dictionary
         such as the phonebook or calendar.
 
         @param tablename: name of the table to use
-        @param dict: The dictionary of values.  The key is ignored, and
-                     the value of each key is treated as a record
+        @param dict: The dictionary of record.  The key must be the uniqueid for each record.
+                   The @L{extractbpserials} function can do the conversion for you for
+                   phonebook and similar formatted records.
         """
+
+        # work on a copy of dict
+        dict=dict.copy()
         
         # make sure the table exists first
         if not self.doestableexist(tablename):
             # create table and include meta-fields
-            self.sql("create table %s (__rowid__ integer primary key, __timestamp__ TIMESTAMP, __deleted__ integer, __uid__ varchar)" % (sqlquote(tablename),))
+            self.sql("create table %s (__rowid__ integer primary key, __timestamp__ TIMESTAMP, __deleted__ integer, __uid__ varchar)" % (idquote(tablename),))
         # examine the keys in dict
         dk=[]
         for k in dict.keys():
@@ -145,7 +165,7 @@ class Database:
         # verify that they don't start with __
         assert len([k for k in dk if k.startswith("__")])==0
         # get database keys
-        dbkeys=[name for _, name in self.getcolumns(tablename)]
+        dbkeys=[name for _, name, _ in self.getcolumns(tablename)]
         # are any missing?
         missing=[k for k in dk if k not in dbkeys]
         if len(missing):
@@ -179,39 +199,64 @@ class Database:
                     continue
             if len(creates):
                 # alter the table to add the new fields - sqlite doesn't support alter table so we do it manually
-                dbtkeys=[(name,type) for name, type, in self.getcolumns(tablename)]
-                cmd=["create", "temporary", "table", sqlquote("backup_"+tablename), "("]
+                dbtkeys=[(name,type) for _, name, type, in self.getcolumns(tablename)]
+                cmd=["create", "temporary", "table", idquote("backup_"+tablename), "("]
                 for n,t in dbtkeys:
                     if cmd[-1]!="(":
                         cmd.append(",")
-                    cmd.append(sqlquote(n))
+                    cmd.append(idquote(n))
                     cmd.append(t)
                 cmd.append(")")
                 self.sql(" ".join(cmd))
                 # copy the values into the temporary table
-                self.sql("insert into %s select * from %s" % (sqlquote("backup_"+tablename), sqlquote(tablename)))
+                self.sql("insert into %s select * from %s" % (idquote("backup_"+tablename), idquote(tablename)))
                 # drop the source table
-                self.sql("drop table %s" % (sqlquote(tablename),))
+                self.sql("drop table %s" % (idquote(tablename),))
                 # recreate the source table with new columns
                 del cmd[1] # remove temporary
-                cmd[2]=sqlquote(tablename) # change tablename
+                cmd[2]=idquote(tablename) # change tablename
                 del cmd[-1] # remove trailing )
                 for n,t in creates:
-                    cmd.extend((',', sqlquote(n), t))
+                    cmd.extend((',', idquote(n), t))
                 cmd.append(')')
                 self.sql(" ".join(cmd))
                 # put values back in
-                cmd=["insert into", sqlquote(tablename), '(']
+                cmd=["insert into", idquote(tablename), '(']
                 for n,_ in dbtkeys:
                     if cmd[-1]!="(":
                         cmd.append(",")
-                    cmd.append(sqlquote(n))
-                cmd.extend([")", "select * from", sqlquote("backup_"+tablename)])
+                    cmd.append(idquote(n))
+                cmd.extend([")", "select * from", idquote("backup_"+tablename)])
                 self.sql(" ".join(cmd))
 
-        # now we have to write the values out
-        dict=dict.copy()
-        dbtkeys=[(name,type) for name, type in self.getcolumns(tablename)]
+        # get the latest values for each guid ...
+        current=self.getmajordictvalues(tablename)
+        # compare what we have, and update/mark deleted as appropriate ...
+        deleted=[k for k in current if k not in dict]
+        new=[k for k in dict if k not in current]
+        modified=[k for k in dict if k in current] # only potentially modified ...
+
+        # deal with modified first
+        dl=[]
+        for i,k in enumerate(modified):
+            if dict[k]==current[k]:
+                # unmodified!
+                del dict[k]
+                dl.append(i)
+        dl.reverse()
+        for i in dl:
+            del modified[i]
+
+        # add deleted entries back into dict
+        for d in deleted:
+            assert d not in deleted
+            dict[d]=current[d]
+            dict[d]["__deleted__"]=True
+            
+        # now we only have new, changed and deleted entries left in dict
+        
+        # write out indirect values
+        dbtkeys=[(name,type) for _, name, type in self.getcolumns(tablename)]
         # for every indirect, we have to replace the value with a pointer
         for n,t in dbtkeys:
             if t=="indirect":
@@ -225,8 +270,20 @@ class Database:
                     self.updateindirecttable("phonebook__"+n, indirects)
                     for r in indirects.keys():
                         dict[r][n]=indirects[r]
-        # get the latest values for each guid ...
-        # compare what we have, and update/mark deleted as appropriate ...
+
+        # and now the main table
+        for k in dict.keys():
+            record=dict[k]
+            record["__uid__"]=k
+            rk=record.keys()
+            rk.sort()
+            cmd=["insert into", idquote(tablename), "("]
+            cmd.append(",".join([idquote(r) for r in rk]))
+            cmd.extend([")", "values", "("])
+            cmd.append(",".join(["?" for r in rk]))
+            cmd.append(")")
+            self.sql(" ".join(cmd), [record[r] for r in rk])
+        
         self.sql("COMMIT")
 
 
@@ -238,7 +295,7 @@ class Database:
         # does the table even exist?
         if not self.doestableexist(tablename):
             # create table and include meta-fields
-            self.sql("create table %s (__rowid__ integer primary key)" % (sqlquote(tablename),))
+            self.sql("create table %s (__rowid__ integer primary key)" % (idquote(tablename),))
         # get the list of keys from indirects
         datakeys=[]
         for i in indirects.keys():
@@ -250,61 +307,62 @@ class Database:
                         assert not k.startswith("__")
                         datakeys.append(k)
         # get the keys from the table
-        dbkeys=[name for name, _ in self.getcolumns(tablename)]
+        dbkeys=[name for _, name, _ in self.getcolumns(tablename)]
         # are any missing?
         missing=[k for k in datakeys if k not in dbkeys]
         if len(missing):
-            dbtkeys=[(name,type) for name, type in self.getcolumns(tablename)]
-            cmd=["create", "temporary", "table", sqlquote("backup_"+tablename), "("]
+            dbtkeys=[(name,type) for _, name, type in self.getcolumns(tablename)]
+            cmd=["create", "temporary", "table", idquote("backup_"+tablename), "("]
             for n,t in dbtkeys:
                 if cmd[-1]!="(":
                     cmd.append(",")
-                cmd.append(sqlquote(n))
+                cmd.append(idquote(n))
                 cmd.append(t)
             cmd.append(")")
             self.sql(" ".join(cmd))
             # copy the values into the temporary table
-            self.sql("insert into %s select * from %s" % (sqlquote("backup_"+tablename), sqlquote(tablename)))
+            self.sql("insert into %s select * from %s" % (idquote("backup_"+tablename), idquote(tablename)))
             # drop the source table
-            self.sql("drop table %s" % (sqlquote(tablename),))
+            self.sql("drop table %s" % (idquote(tablename),))
             # recreate the source table with new columns
             del cmd[1] # remove temporary
-            cmd[2]=sqlquote(tablename) # change tablename
+            cmd[2]=idquote(tablename) # change tablename
             del cmd[-1] # remove trailing )
             for m in missing:
-                cmd.extend((',', sqlquote(m), "value"))
+                cmd.extend((',', idquote(m), "value"))
             cmd.append(')')
             self.sql(" ".join(cmd))
             # put values back in
-            cmd=["insert into", sqlquote(tablename), '(']
+            cmd=["insert into", idquote(tablename), '(']
             for n,_ in dbtkeys:
                 if cmd[-1]!="(":
                     cmd.append(",")
-                cmd.append(sqlquote(n))
-            cmd.extend([")", "select * from", sqlquote("backup_"+tablename)])
+                cmd.append(idquote(n))
+            cmd.extend([")", "select * from", idquote("backup_"+tablename)])
             self.sql(" ".join(cmd))
         # for each row we now work out the indirect information
         for r in indirects:
             res=tablename+","
             for record in indirects[r]:
-                cmd=["select __rowid__ from", sqlquote(tablename), "where"]
+                cmd=["select __rowid__ from", idquote(tablename), "where"]
                 params=[]
                 for d in datakeys:
                     if cmd[-1]!="where":
                         cmd.append("and")
                     v=record.get(d,None)
                     if v is None:
-                        cmd.extend([sqlquote(d), "isnull"])
+                        cmd.extend([idquote(d), "isnull"])
                     else:
-                        cmd.extend([sqlquote(d), "=", "?"])
+                        cmd.extend([idquote(d), "=", "?"])
                         params.append(v)
                 found=None
                 for found in self.sql(" ".join(cmd), params):
                     # get matching row
+                    found=found[0]
                     break
                 if found is None:
                     # add it
-                    cmd=["insert into", sqlquote(tablename), "("]
+                    cmd=["insert into", idquote(tablename), "("]
                     params=[]
                     for k in record:
                         if cmd[-1]!="(":
@@ -321,8 +379,80 @@ class Database:
             print  "indirects[",r,"]=",res
                         
 
+    def getmajordictvalues(self, tablename):
+        if not self.doestableexist(tablename):
+            return {}
+
+        res={}
+        uids=[u[0] for u in self.sql("select distinct __uid__ from %s" % (idquote(tablename),))]
+        desc={}
+        desc[tablename]=[d for d in self.getcolumns(tablename)]
+        for colnum,name,type in desc[tablename]:
+            if name=='__deleted__':
+                deleted=colnum
+            elif name=='__uid__':
+                uid=colnum
+        for row in self.sqlmany("select * from %s where __uid__=? order by __rowid__ desc limit 1" % (idquote(tablename),), [(u,) for u in uids]):
+            if row[deleted]:
+                continue
+            record={}
+            for colnum,name,type in desc[tablename]:
+                if name.startswith("__") or type not in ("value", "indirect") or row[colnum] is None:
+                    continue
+                if type=="value":
+                    record[name]=row[colnum]
+                    continue
+                assert type=="indirect"
+                record[name]=self.getindirect(row[colnum], desc)
+            res[row[uid]]=record
+        return res
+
+    def getindirect(self, what, schemas=None):
+        """Gets a list of values (indirect) as described by what
+        @param what: what to get - eg phonebook_serials,1,3,5,
+                      (note there is always a trailing comma)
+        @param schemas: a dict holding the schemas for various tables.  It is useful to provide
+                     this if you repeatedly call this function
+        """
+        if schemas is None:
+            schemas={}
+
+        tablename,rows=schemas.split(',', 1)
+        if tablename not in schemas:
+            schemas[tablename]=[d for d in self.getcolumns(tablename)]
+
+        res=[]
+        for row in self.sqlmany("select * from %s where __rowid__=?" % (idquote(tablename),), [(int(r),) for r in rows.split(',') if len(r)]):
+            record={}
+            for colnum,name,type in schemas[tablename]:
+                if name.startswith("__") or type not in ("value", "indirect") or row[colnum] is None:
+                    continue
+                if type=="value":
+                    record[name]=row[colnum]
+                    continue
+                assert type=="indirect"
+                assert False, "indirect in indirect no handled"
+            assert len(record)
+            res.append(record)
+        assert len(res)
+        return res
         
-    savemajordict=ExclusiveWrapper(savemajordict)
+    # savemajordict=ExclusiveWrapper(savemajordict)
+
+def extractbpserials(dict):
+    """Changes dict keys to be the bitpim serial for each row"""
+    res={}
+    
+    for r in dict.keys():
+        found=False
+        record=dict[r]
+        for v in record["serials"]:
+            if v["sourcetype"]=="bitpim":
+                found=True
+                res[v["id"]]=record
+                break
+        assert found
+    return res
 
 
 if __name__=='__main__':
@@ -334,7 +464,7 @@ if __name__=='__main__':
         # use the phonebook out of the examples directory
         execfile("examples/phonebook-index.idx")
 
-        db.savemajordict("phonebook", phonebook)
+        db.savemajordict("phonebook", extractbpserials(phonebook))
     except:
         print common.formatexception()
         
