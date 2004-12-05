ffe527b95013b91746a136405f5bb14a2f184cf3
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-12-05 05:36:32 +0000

    Disallow writing media to the phone.  The Bell 8100 is crippled so that uploaded media can not be viewed, used or deleted, so there is no point allowing uploads.
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1800 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_sanyo8100_bell.py
+++ src/phones/com_sanyo8100_bell.py
@@ -63,10 +63,10 @@ class Profile(com_sanyonewer.Profile):
         ('calendar', 'read', None),   # all calendar reading
         ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
         ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
-        ('wallpaper', 'write', 'MERGE'),
-        ('ringtone', 'write', 'MERGE'),
         ('wallpaper', 'read', None),  # all wallpaper reading
         ('ringtone', 'read', None),   # all ringtone reading
+        # ('wallpaper', 'write', 'MERGE'),
+        # ('ringtone', 'write', 'MERGE'),
     )
 
     def __init__(self):
