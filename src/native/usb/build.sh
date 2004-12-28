67c17f0f1a7acbbeb4a45271af34172c9464bb57
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-12-28 08:41:59 +0000

    make it easier to build against a non-standard python installation
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1860 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/native/usb/build.sh
+++ src/native/usb/build.sh
@@ -1,8 +1,7 @@
 PYTHONVER=python2.3
-INCLUDEDIR=/usr/include/$PYTHONVER
-LIBDIR=/usr/lib/$PYTHONVER/config
+INCLUDEDIR=`$PYTHONVER -c "import distutils.sysconfig; print distutils.sysconfig.get_python_inc()"`
 
 swig -python -I/usr/include libusb.i
 
-gcc -Wall -O2  -shared  -I $INCLUDEDIR -L $LIBDIR -o _libusb.so libusb_wrap.c -lusb 
+gcc -Wall -fno-strict-aliasing -O2 -g  -shared  -I $INCLUDEDIR -o _libusb.so libusb_wrap.c -lusb 
 strip _libusb.so
