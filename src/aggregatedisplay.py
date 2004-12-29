7082fa8e2885799bb56884825a772a723d00cd4b
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2004-12-29 10:31:00 +0000

    - add padding around items - space items horizontally to avoid unused space on right - only draw visible items - seperate function to do layout and function to query data - various other small tweaks
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1870 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/aggregatedisplay.py
+++ src/aggregatedisplay.py
@@ -16,9 +16,11 @@ import guihelper
 class Display(wx.ScrolledWindow):
     "This is the view"
 
-    # See the long note in OnPaint before touching this
+    # See the long note in OnPaint before touching this code
 
     VSCROLLPIXELS=1 # how many pixels we scroll by
+
+    ITEMPADDING=5   # how many pixels we pad items by
     
     def __init__(self, parent, datasource, watermark=None):
         wx.ScrolledWindow.__init__(self, parent, id=wx.NewId(), style=wx.FULL_REPAINT_ON_RESIZE)
@@ -42,7 +44,7 @@ class Display(wx.ScrolledWindow):
         else:
             self.watermark=None
         self.relayoutpending=False
-        self.ReLayout()
+        self.UpdateItems()
 
     def OnScroll(self, evt):
         self.Refresh(False)
@@ -90,34 +92,54 @@ class Display(wx.ScrolledWindow):
         if self._bufbmp is None or self._bufbmp.GetWidth()<self._w or self._bufbmp.GetHeight()<self.maxheight:
             self._bufbmp=wx.EmptyBitmap(self._w, self.maxheight)
         dc=wx.BufferedPaintDC(self, self._bufbmp)
+        # we redraw everything that is in the visible area of the screen
         origin=self.GetViewStart()[1]*self.VSCROLLPIXELS
+        firstvisible=origin
+        lastvisible=origin+self._h
+
+        # clear background
         dc.SetBackground(self.bgbrush)
         dc.Clear()
         # draw watermark
         if self.watermark:
-            # place in the middle
+            # place in the middle:
             # dc.DrawBitmap(self.watermark, self._w/2-self.watermark.GetWidth()/2, origin+self._h/2-self.watermark.GetHeight()/2, True)
-            # place in bottom right
+            # place in bottom right:
             dc.DrawBitmap(self.watermark, self._w-self.watermark.GetWidth(), origin+self._h-self.watermark.GetHeight(), True)
 
         # draw each section
         cury=0
         for i, section in enumerate(self.sections):
-            dc.SetDeviceOrigin(0, cury )
-            section.Draw(dc, self._w)
+            if _isvisible(cury, cury+self.sectionheights[i], firstvisible, lastvisible):
+                dc.SetDeviceOrigin(0, cury )
+                section.Draw(dc, self._w)
             cury+=self.sectionheights[i]
+            extrawidth=(self._w-6)-(self.itemsize[i][0]+self.ITEMPADDING)*self.itemsperrow[i]
+            extrawidth/=self.itemsperrow[i]
+            if extrawidth<0: extrawidth=0
             # now draw items in this section
-            for num,item in enumerate(self.items[i]):
-                dc.SetDeviceOrigin((num%self.itemsperrow[i])*self.itemsize[i][0], cury+(num/self.itemsperrow[i])*self.itemsize[i][1])
+            num=0
+            while num<len(self.items[i]):
+                x=(num%self.itemsperrow[i])
+                y=(num/self.itemsperrow[i])
+                posy=cury+y*(self.itemsize[i][1]+self.ITEMPADDING)
+                # skip the entire row if it isn't visible
+                if x==0 and not _isvisible(posy, posy+self.itemsize[i][1], firstvisible, lastvisible):
+                    num+=self.itemsperrow[i]
+                    continue
+                item=self.items[i][num]
+                dc.SetDeviceOrigin(3+x*(self.itemsize[i][0]+self.ITEMPADDING+extrawidth), posy)
                 dc.ResetBoundingBox()
-                item.Draw(dc, self.itemsize[i][0], self.itemsize[i][1], False)
+                item.Draw(dc, self.itemsize[i][0]+extrawidth, self.itemsize[i][1], False)
                 #print "bb",dc.MinX(), dc.MinY(), dc.MaxX(), dc.MaxY()
-            cury+=(len(self.items[i])+self.itemsperrow[i]-1)/self.itemsperrow[i]*self.itemsize[i][1]
+                num+=1
+            cury+=(len(self.items[i])+self.itemsperrow[i]-1)/self.itemsperrow[i]*(self.itemsize[i][1]+self.ITEMPADDING)
                     
         # must do this or the world ends ...
         dc.SetDeviceOrigin(0,0)
 
     def ReLayout(self):
+        "Called if the window size has changed"
         if self.relayoutpending:
             return
         self.relayoutpending=True
@@ -125,33 +147,41 @@ class Display(wx.ScrolledWindow):
 
     def _ReLayout(self):
         self.relayoutpending=False
-        self.sections=self.datasource.GetSections()
-        self.items=[]
-        #self.itemsize=self.datasource.GetItemSize()
-        #self.itemsperrow=max(self._w/self.itemsize[0],1)
         self.itemsperrow=[]
-        self.itemsize=[]
-        self.sectionheights=[]
         self.vheight=0
         for i,section in enumerate(self.sections):
-            self.sectionheights.append(section.GetHeight())
             self.vheight+=self.sectionheights[-1]
-            self.itemsize.append(self.datasource.GetItemSize(i,section))
-            items=self.datasource.GetItemsFromSection(i,section)
-            self.itemsperrow.append(max(self._w/self.itemsize[-1][0],1))
-            rows=(len(items)+self.itemsperrow[-1]-1)/self.itemsperrow[-1]
-            self.vheight+=rows*self.itemsize[-1][1]
-            self.items.append(items)
-
-        # You can't adjust self._w here to take into account the presence of the
-        # scrollbar since a size event is generated after the call to setscrollbars
-        # so we do the adjustment in the size handler.
+            self.itemsperrow.append(max((self._w-6)/(self.itemsize[i][0]+self.ITEMPADDING),1))
+            rows=(len(self.items[i])+self.itemsperrow[i]-1)/self.itemsperrow[i]
+            self.vheight+=rows*(self.itemsize[i][1]+self.ITEMPADDING)
 
         # set the height we want wx to think the window is
         self.maxheight=max(self.vheight,self._h)
         # adjust scrollbar
         self.SetScrollbars(0,1,0,self.vheight,0, self.GetViewStart()[1])
         self.SetScrollRate(0, self.VSCROLLPIXELS)
+
+    def UpdateItems(self):
+        "Called if you want the items to be refetched from the model"
+        self.sections=self.datasource.GetSections()
+        self.items=[]
+        self.itemsize=[]
+        self.sectionheights=[]
+        for i,section in enumerate(self.sections):
+            self.sectionheights.append(section.GetHeight())
+            self.itemsize.append(self.datasource.GetItemSize(i,section))
+            items=self.datasource.GetItemsFromSection(i,section)            
+            self.items.append(items)
+        # we have to relayout immediately otherwise there could a be paint call
+        # between this function and relayout and the variables would be in an
+        # inconsistent state
+        self._ReLayout()
+            
+def _isvisible(start, end, firstvisible, lastvisible):
+    return start <= firstvisible <= end <= lastvisible or \
+           (start >= firstvisible and start <= lastvisible) or \
+           (start <= firstvisible and end >=lastvisible)
+
 
 class SectionHeader(object):
     "A generic section header implementation"
@@ -182,10 +212,10 @@ class SectionHeader(object):
         dc.SetFont(self.font2)
         for xoff in (0,1,2):
             for yoff in (0,1,2):
-                dc.DrawText(self.label, xoff, yoff)
+                dc.DrawText(self.label, xoff+3, yoff)
         dc.SetTextForeground(self.fontcolour)
         dc.SetFont(self.font)
-        dc.DrawText(self.label, 1,1)
+        dc.DrawText(self.label, 1+3,1)
         dc.SetTextForeground(oldc)
         dc.SetFont(oldf)
                 
@@ -235,6 +265,9 @@ class DataSource(object):
 if __name__=="__main__":
     app=wx.PySimpleApp()
 
+    import sys
+    import common
+    sys.excepthook=common.formatexceptioneh
     import wx.html
     import os
     import brewcompressedimage
@@ -252,7 +285,7 @@ if __name__=="__main__":
     imagespath=p
     images=[name for name in os.listdir(imagespath) if name[-4:] in (".bci", ".bmp", ".jpg", ".png")]
     images.sort()
-    images=images[:9]
+    #images=images[:9]
 
     print imagespath
     print images
@@ -288,6 +321,9 @@ if __name__=="__main__":
             self.itemnum=itemnum
 
         def Draw(self, dc, width, height, selected):
+            # uncomment to see exactly what size is given
+            #dc.DrawRectangle(0,0,width,height)
+
             us=dc.GetUserScale()
             dc.SetClippingRegion(0,0,width,height)
             hdc=wx.html.HtmlDCRenderer()
