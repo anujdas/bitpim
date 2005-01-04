1522f9d38051d3ac3622f57defabe50ead3a7902
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-01-04 02:01:15 +0000

    Get the model name of the A620 right.  SPH not SCH.
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1903 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/guiwidgets.py
+++ src/guiwidgets.py
@@ -337,7 +337,7 @@ class ConfigDialog(wx.Dialog):
                   'SCP-8100': 'com_sanyo8100',
                   'SCP-8100 (Bell Mobility)': 'com_sanyo8100_bell',
                   'SCH-A310': 'com_samsungscha310',
-                  'SCH-A620': 'com_samsungscha620',
+                  'SPH-A620 (VGA1000)': 'com_samsungspha620',
                   'SCH-A650': 'com_samsungscha650',
                   'SCH-A670': 'com_samsungscha670',
                   'Other CDMA phone': 'com_othercdma',
