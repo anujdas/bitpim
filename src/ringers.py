9312a82af70ba7d805b041b88095d0e39333162b
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-01-09 11:25:01 +0000

    add in Organize by menu
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@1938 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/ringers.py
+++ src/ringers.py
@@ -36,13 +36,24 @@ class RingerView(guiwidgets.FileView):
     # from being GC while any instance of this class exists
     __publisher=pubsub.Publisher
 
+
+    organizetypes=("Audio Type", "Origin", "File Size")
     
     def __init__(self, mainwindow, parent, id=-1):
         self.mainwindow=mainwindow
         self._data={'ringtone-index': {}}
         self.updateprofilevariables(self.mainwindow.phoneprofile)
+        self.organizemenu=wx.Menu()
         guiwidgets.FileView.__init__(self, mainwindow, parent, "ringtone-watermark")
         self.wildcard="Audio files|*.wav;*.mid;*.qcp;*.mp3|Midi files|*.mid|Purevoice files|*.qcp|MP3 files|*.mp3|All files|*.*"
+
+        self.organizeinfo={}
+
+        for k in self.organizetypes:
+            id=wx.NewId()
+            self.organizemenu.AppendRadioItem(id, k)
+            wx.EVT_MENU(self, id, self.OrganizeChange)
+            self.organizeinfo[id]=getattr(self, "organizeby_"+k.replace(" ",""))
 
         self.modified=False
         wx.EVT_IDLE(self, self.OnIdle)
@@ -75,29 +86,106 @@ class RingerView(guiwidgets.FileView):
         assert w==self.thumbnail.GetWidth() and h==self.thumbnail.GetHeight()
         return self.thumbnail
 
+    def OrganizeChange(self, evt):
+        evt.GetEventObject().Check(evt.GetId(), True)
+        self.OnRefresh()
+
     def GetSections(self):
-        x=aggregatedisplay.SectionHeader("Ringtones")
+        # work out section and item sizes
         self.thumbnail=wx.Image(guihelper.getresourcefile('ringer.png')).ConvertToBitmap()
-        x.thumbsize=(self.thumbnail.GetWidth(), self.thumbnail.GetHeight())
+        
         dc=wx.MemoryDC()
-        dc.SelectObject(wx.EmptyBitmap(100,100))
+        dc.SelectObject(wx.EmptyBitmap(100,100)) # unused bitmap needed to keep wxMac happy
         h=dc.GetTextExtent("I")[1]
-        x.itemsize=x.thumbsize[0]+140, max(x.thumbsize[1], h*4+DisplayItem.PADDING)+DisplayItem.PADDING*2
-        return [x]
+        itemsize=self.thumbnail.GetWidth()+140, max(self.thumbnail.GetHeight(), h*4+DisplayItem.PADDING)+DisplayItem.PADDING*2
+        
+        # get all the items
+        items=[DisplayItem(self, key, self.mainwindow.ringerpath) for key in self._data['ringtone-index']]
+        # prune out ones we don't have images for
+        items=[item for item in items if os.path.exists(item.filename)]
+
+        self.sections=[]
+
+        if len(items)==0:
+            return self.sections
+        
+        # get the current sorting type
+        for i in range(len(self.organizetypes)):
+            item=self.organizemenu.FindItemByPosition(i)
+            if self.organizemenu.IsChecked(item.GetId()):
+                for sectionlabel, items in self.organizeinfo[item.GetId()](items):
+                    sh=aggregatedisplay.SectionHeader(sectionlabel)
+                    sh.itemsize=itemsize
+                    for item in items:
+                        item.thumbnailsize=self.thumbnail.GetWidth(), self.thumbnail.GetHeight()
+                    # sort items by name
+                    items=[(item.name.lower(), item) for item in items]
+                    items.sort()
+                    items=[item for name,item in items]
+                    self.sections.append( (sh, items) )
+                return [sh for sh,items in self.sections]
+        assert False, "Can't get here"
+
+    def GetItemsFromSection(self, sectionnumber, sectionheader):
+        return self.sections[sectionnumber][1]
+
+    def organizeby_AudioType(self, items):
+        types={}
+        for item in items:
+            t=item.fileinfo.format
+            if t is None: t="<Unknown>"
+            l=types.get(t, [])
+            l.append(item)
+            types[t]=l
+
+        keys=types.keys()
+        keys.sort()
+        return [ (key, types[key]) for key in types]
+
+    def organizeby_Origin(self, items):
+        types={}
+        for item in items:
+            t=item.origin
+            if t is None: t="Default"
+            l=types.get(t, [])
+            l.append(item)
+            types[t]=l
+
+        keys=types.keys()
+        keys.sort()
+        return [ (key, types[key]) for key in types]
+        
+    def organizeby_FileSize(self, items):
+        
+        sizes={0: ('Less than 8kb', []),
+               8192: ('8 kilobytes', []),
+               16384: ('16 kilobytes', []),
+               32768: ('32 kilobytes', []),
+               65536: ('64 kilobytes', []),
+               131052: ('128 kilobytes', []),
+               524208: ('512 kilobytes', []),
+               1024*1024: ('One megabyte', [])}
+
+        keys=sizes.keys()
+        keys.sort()
+
+        for item in items:
+            t=item.size
+            if t>=keys[-1]:
+                sizes[keys[-1]][1].append(item)
+                continue
+            for i,k in enumerate(keys):
+                if t<keys[i+1]:
+                    sizes[k][1].append(item)
+                    break
+
+        return [sizes[k] for k in keys if len(sizes[k][1])]   
 
     def GetItemSize(self, sectionnumber, sectionheader):
         return sectionheader.itemsize
 
     def GetFileInfo(self, filename):
         return fileinfo.identify_audiofile(filename)
-
-    def GetItemsFromSection(self, sectionnumber, sectionheader):
-        items=[DisplayItem(self, key, self.mainwindow.ringerpath) for key in self._data['ringtone-index']]
-        # prune out ones we don't have files for
-        items=[item for item in items if os.path.exists(item.filename)]
-        for item in items:
-            item.setthumbnailsize(sectionheader.thumbsize)
-        return items
 
     def RemoveFromIndex(self, names):
         for name in names:
