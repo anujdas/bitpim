04eb6511c2ce32de56d82e0e63a99cf8a42ce49f
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-05-03 04:45:19 +0000

    bump version number
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2357 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/version.py
+++ src/version.py
@@ -12,7 +12,7 @@
 import time
 
 name="BitPim"
-version="0.7.31"
+version="0.7.32"
 vendor="official"
 release=0  # when rereleases of the same version happen, this gets incremented
 testver=0  # value of zero is non-test build
