9b1b45b1e7ac792113b296f931eeb8e1ada33b6c
djpham <djpham@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-09-13 12:06:35 +0000

    Updated auto-detect of LG phones
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2518 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_lgvx4400.py
+++ src/phones/com_lgvx4400.py
@@ -781,7 +781,22 @@ class Phone(com_phone.Phone,com_brew.BrewProtocol,com_lg.LGPhonebook,com_lg.LGIn
 
         return e
 
-    is_mode_brew=com_brew.BrewProtocol._setmodebrew
+    def is_mode_brew(self):
+        print 'is_mode_brew'
+        req=p_brew.memoryconfigrequest()
+        respc=p_brew.memoryconfigresponse
+        
+        for baud in 0, 38400, 115200:
+            if baud:
+                if not self.comm.setbaudrate(baud):
+                    continue
+            try:
+                self.sendbrewcommand(req, respc, callsetmode=False)
+                return True
+            except com_phone.modeignoreerrortypes:
+                pass
+        return False
+
     brew_version_txt_key='brew_version.txt'
     brew_version_file='brew/version.txt'
     lgpbinfo_key='lgpbinfo'
