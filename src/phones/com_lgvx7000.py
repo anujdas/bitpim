b68424efeb359cb8d21df0c4cbc550a74d23a3f1
djpham <djpham@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-09-16 02:25:14 +0000

    Updated LG VX-7000 phone code
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2525 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_lgvx7000.py
+++ src/phones/com_lgvx7000.py
@@ -104,13 +104,16 @@ class Profile(parentprofile):
 
     _supportedsyncs=(
         ('phonebook', 'read', None),  # all phonebook reading
-        # ('calendar', 'read', None),   # all calendar reading
+        ('calendar', 'read', None),   # all calendar reading
         ('wallpaper', 'read', None),  # all wallpaper reading
         ('ringtone', 'read', None),   # all ringtone reading
+        ('call_history', 'read', None),# all call history list reading
+        ('sms', 'read', None),         # all SMS list reading
         ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
-        # ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
+        ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
         ('wallpaper', 'write', 'MERGE'),      # merge and overwrite wallpaper
         ('wallpaper', 'write', 'OVERWRITE'),
         ('ringtone', 'write', 'MERGE'),      # merge and overwrite ringtone
         ('ringtone', 'write', 'OVERWRITE'),
+        ('sms', 'write', 'OVERWRITE'),        # all SMS list writing
         )
