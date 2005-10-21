068ebea78e7ee86c76bad7f2d6819c8b3a2e36d7
skyjunky <skyjunky@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-10-21 06:07:08 +0000

    Add protocolclass to other phone (for new brew filesystem)
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2572 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_othercdma.py
+++ src/phones/com_othercdma.py
@@ -11,11 +11,14 @@
 
 import com_phone
 import com_brew
+import commport
 
 class Phone(com_phone.Phone, com_brew.BrewProtocol):
     "Talk to an unsupported CDMA phone"
 
     desc="Other CDMA Phone"
+
+    protocolclass=None
     
     def __init__(self, logtarget, commport):
         com_phone.Phone.__init__(self, logtarget, commport)
