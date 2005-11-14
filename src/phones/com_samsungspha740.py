60228f41f6df0299ca5e036aacbb75cf87ca445a
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-11-14 04:39:01 +0000

    Same as A620 except that camera files in a different location.
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2619 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/phones/com_samsungspha740.py
@@ -0,0 +1,81 @@
+### BITPIM
+###
+### Copyright (C) 2005 Stephen Wood <saw@bitpim.org>
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the BitPim license as detailed in the LICENSE file.
+###
+### $Id$
+
+"""Communicate with a Samsung SPH-A740"""
+
+import sha
+import re
+import struct
+
+import common
+import commport
+import p_brew
+import p_samsungspha620
+import p_samsungspha740
+import com_brew
+import com_phone
+import com_samsung_packet
+import com_samsungspha620
+import prototypes
+
+numbertypetab=('home','office','cell','pager','fax','none')
+
+class Phone(com_samsungspha620.Phone):
+    "Talk to a Samsung SPH-A740 phone"
+
+    desc="SPH-A740"
+
+    protocolclass=p_samsungspha740
+    serialsname='spha740'
+
+    # jpeg Remove first 124 characters
+
+    imagelocations=(
+        # offset, index file, files location, origin, maximumentries, header offset
+        # Offset is arbitrary.  100 is reserved for amsRegistry indexed files
+        (400, "cam/dldJpeg", "camera", 100, 124),
+        (300, "cam/jpeg", "camera", 100, 124),
+        )
+
+    ringtonelocations=(
+        # offset, index file, files location, type, maximumentries, header offset
+        )
+        
+
+    def __init__(self, logtarget, commport):
+        com_samsungspha620.Phone.__init__(self, logtarget, commport)
+        self.numbertypetab=numbertypetab
+        self.mode=self.MODENONE
+
+parentprofile=com_samsungspha620.Profile
+class Profile(parentprofile):
+    deviceclasses=("modem",)
+
+    protocolclass=Phone.protocolclass
+    serialsname=Phone.serialsname
+    phone_manufacturer='SAMSUNG'
+    phone_model='SPH-A740/152'
+
+    def __init__(self):
+        parentprofile.__init__(self)
+        self.numbertypetab=numbertypetab
+
+    _supportedsyncs=(
+        ('phonebook', 'read', None),  # all phonebook reading
+        ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
+        ('wallpaper', 'read', None),  # all wallpaper reading
+        ('ringtone', 'read', None),   # all ringtone reading
+        ('calendar', 'read', None),   # all calendar reading
+        ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
+        ('todo', 'read', None),     # all todo list reading
+        ('todo', 'write', 'OVERWRITE'),  # all todo list writing
+        ('memo', 'read', None),     # all memo list reading
+        ('memo', 'write', 'OVERWRITE'),  # all memo list writing
+        )
+
