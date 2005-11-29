8bef45f0701bbffb9d490711394d65137a77603d
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2005-11-29 03:02:39 +0000

    Use default supportedsyncs from sanyo_newer
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2655 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_sanyo8300.py
+++ src/phones/com_sanyo8300.py
@@ -140,17 +140,6 @@ class Profile(com_sanyonewer.Profile):
     WALLPAPER_WIDTH=176
     WALLPAPER_HEIGHT=220
 
-    _supportedsyncs=(
-        ('phonebook', 'read', None),  # all phonebook reading
-        ('calendar', 'read', None),   # all calendar reading
-        ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
-        ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
-        ('wallpaper', 'write', 'MERGE'),
-        ('ringtone', 'write', 'MERGE'),
-        ('wallpaper', 'read', None),  # all wallpaper reading
-        ('ringtone', 'read', None),   # all ringtone reading
-    )
-
     def __init__(self):
         com_sanyonewer.Profile.__init__(self)
         self.numbertypetab=numbertypetab
