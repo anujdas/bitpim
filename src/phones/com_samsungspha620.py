aeb81fd804e20704008431b403ff6330dbcde3c9
sawecw <sawecw@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2006-01-01 04:11:55 +0000

    Add ability to write ringtones to phone. Writing wallpaper needs work on image conversion.
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2709 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/phones/com_samsungspha620.py
+++ src/phones/com_samsungspha620.py
@@ -22,6 +22,7 @@ import com_brew
 import com_phone
 import com_samsung_packet
 import prototypes
+import fileinfo
 
 numbertypetab=('home','office','cell','pager','fax','none')
 
@@ -32,7 +33,6 @@ class Phone(com_samsung_packet.Phone):
 
     protocolclass=p_samsungspha620
     serialsname='spha620'
-    __groups_range=xrange(5)
 
     # digital_cam/jpeg Remove first 148 characters
 
@@ -47,6 +47,8 @@ class Phone(com_samsung_packet.Phone):
         # offset, index file, files location, type, maximumentries, header offset
         )
         
+    __audio_mimetype={ 'mid': 'audio/midi', 'qcp': 'audio/vnd.qcelp', 'pmd': 'application/x-pmd'}
+    __image_mimetype={ 'jpg': 'image/jpg', 'jpeg': 'image/jpeg', 'gif': 'image/gif', 'bmp': 'image/bmp', 'png': 'image/png'}
 
     def __init__(self, logtarget, commport):
         com_samsung_packet.Phone.__init__(self, logtarget, commport)
@@ -60,15 +62,11 @@ class Phone(com_samsung_packet.Phone):
         # use a hash of ESN and other stuff (being paranoid)
         self.log("Retrieving fundamental phone information")
         self.log("Phone serial number")
-        print "Calling setmode MODEMODEM"
         self.setmode(self.MODEMODEM)
-        print "Getting serial number"
         results['uniqueserial']=sha.new(self.get_esn()).hexdigest()
 
         self.log("Reading group information")
-        print "Getting Groups"
         results['groups']=self.read_groups()
-        print "Got Groups"
         # Comment the next to out if you want to read the phonebook
         #self.getwallpaperindices(results)
         #self.getringtoneindices(results)
@@ -296,10 +294,157 @@ class Phone(com_samsung_packet.Phone):
         while contents[i:i+1]!='\0':
             i+=1
         return contents[start:i]
+
+    def makegcd(self, filename,size,mimetable):
+        "Build a GCD file for filename"
+        ext=common.getext(filename.lower())
+        try:
+            mimetype=mimetable[ext]
+        except:
+            return ""
+        
+        noextname=common.stripext(filename)
+        gcdcontent="Content-Type: "+mimetype+"\nContent-Name: "+noextname+"\nContent-Version: 1.0\nContent-Vendor: BitPim\nContent-URL: file:"+filename+"\nContent-Size: "+`size`+"\n\n\n"
+        return gcdcontent
+        
+    def saveringtones(self, result, merge):
+        dircache=self.DirCache(self)
+        media_prefix=self.protocolclass.RINGERPREFIX
+        endtransactionpath=self.protocolclass.ENDTRANSACTION
+        media=result.get('ringtone', {})
+        media_index=result.get('ringtone-index', {})
+        media_names=[x['name'] for x in media.values()]
+        index_names=[x['name'] for x in media_index.values()]
+        if merge:
+            del_names=[]
+        else:
+            del_names=[common.stripext(x) for x in index_names if x not in media_names]
+            gcdpattern=re.compile("[\n\r]Content-Name: +(.*?)[\n\r]")
+        new_names=[x for x in media_names if x not in index_names]
+
+        progressmax=len(del_names)+len(new_names)
+        progresscur=0
+        self.log("Writing ringers")
+        self.progress(progresscur, progressmax, "Writing ringers")
+
+        for icnt in range(1,101):
+            if not (new_names or del_names):
+                break
+            fname=media_prefix+`icnt`
+            fnamegcd=media_prefix+`icnt`+".gcd"
+            fstat=dircache.stat(fnamegcd)
+            if del_names and fstat:
+                # See if this file is in list of files to delete
+                gcdcontents=dircache.readfile(fnamegcd)
+                thisfile=gcdpattern.search(gcdcontents).groups()[0]
+                if thisfile in del_names:
+                    self.log("Deleting ringer "+thisfile)
+                    self.progress(progresscur, progressmax, "Deleting ringer "+thisfile)
+                    progresscur+=1
+                    dircache.rmfile(fname)
+                    dircache.rmfile(fnamegcd)
+                    del_names.remove(thisfile)
+                    fstat=False
+            if new_names and not fstat:
+                newname=new_names.pop()
+                contents=""
+                for k in media.keys():
+                    if media[k]['name']==newname:
+                        contents=media[k]['data']
+                        break
+
+                contentsize=len(contents)
+                if contentsize:
+                    gcdcontents=self.makegcd(newname,contentsize,self.__audio_mimetype)
+                    self.log("Writing ringer "+newname)
+                    self.progress(progresscur, progressmax, "Deleting ringer "+newname)
+                    progresscur+=1
+                    dircache.writefile(fname,contents)
+                    dircache.writefile(fnamegcd,gcdcontents)
+
+        fstat=dircache.stat(endtransactionpath)
+        self.log("Finished writing ringers")
+        self.progress(progressmax, progressmax, "Finished writing ringers")
+        if fstat:
+            dircache.rmfile(endtransactionpath)
+        result['rebootphone']=True
+
+        return
+
+    def savewallpapers(self, result, merge):
+        dircache=self.DirCache(self)
+        media_prefix=self.protocolclass.WALLPAPERPREFIX
+        endtransactionpath=self.protocolclass.ENDTRANSACTION
+        media=result.get('wallpapers', {})
+        media_index=result.get('wallpaper-index', {})
+        media_names=[x['name'] for x in media.values()]
+        index_names=[x['name'] for x in media_index.values()]
+        if merge:
+            del_names=[]
+        else:
+            del_names=[common.stripext(x) for x in index_names if x not in media_names]
+            gcdpattern=re.compile("[\n\r]Content-Name: +(.*?)[\n\r]")
+        new_names=[x for x in media_names if x not in index_names]
+
+        progressmax=len(del_names)+len(new_names)
+        progresscur=0
+        self.log("Writing ringers")
+        self.progress(progresscur, progressmax, "Writing ringers")
+
+        for icnt in range(1,101):
+            if not (new_names or del_names):
+                break
+            fname=media_prefix+`icnt`
+            fnamegcd=media_prefix+`icnt`+".gcd"
+            fstat=dircache.stat(fnamegcd)
+            if del_names and fstat:
+                # See if this file is in list of files to delete
+                gcdcontents=dircache.readfile(fnamegcd)
+                thisfile=gcdpattern.search(gcdcontents).groups()[0]
+                if thisfile in del_names:
+                    self.log("Deleting ringer "+thisfile)
+                    self.progress(progresscur, progressmax, "Deleting ringer "+thisfile)
+                    progresscur+=1
+                    dircache.rmfile(fname)
+                    dircache.rmfile(fnamegcd)
+                    del_names.remove(thisfile)
+                    fstat=False
+            if new_names and not fstat:
+                newname=new_names.pop()
+                contents=""
+                for k in media.keys():
+                    if media[k]['name']==newname:
+                        contents=media[k]['data']
+                        break
+
+                contentsize=len(contents)
+                if contentsize:
+                    gcdcontents=self.makegcd(newname,contentsize,self.__image_mimetype)
+                    self.log("Writing ringer "+newname)
+                    self.progress(progresscur, progressmax, "Deleting ringer "+newname)
+                    progresscur+=1
+                    dircache.writefile(fname,contents)
+                    dircache.writefile(fnamegcd,gcdcontents)
+
+        fstat=dircache.stat(endtransactionpath)
+        self.log("Finished writing ringers")
+        self.progress(progressmax, progressmax, "Finished writing ringers")
+        if fstat:
+            dircache.rmfile(endtransactionpath)
+        result['rebootphone']=True
+
+        return
+
         
 class Profile(com_samsung_packet.Profile):
     protocolclass=Phone.protocolclass
     serialsname=Phone.serialsname
+
+    MAX_RINGTONE_BASENAME_LENGTH=19
+    RINGTONE_FILENAME_CHARS="abcdefghijklmnopqrstuvwxyz0123456789_ ."
+    RINGTONE_LIMITS= {
+        'MAXSIZE': 30000
+    }
     phone_manufacturer='SAMSUNG'
     phone_model='SPH-A620/152'
 
@@ -311,7 +456,9 @@ class Profile(com_samsung_packet.Profile):
         ('phonebook', 'read', None),  # all phonebook reading
         ('phonebook', 'write', 'OVERWRITE'),  # only overwriting phonebook
         ('wallpaper', 'read', None),  # all wallpaper reading
+        #('wallpaper', 'write', None), # Image conversion needs work
         ('ringtone', 'read', None),   # all ringtone reading
+        ('ringtone', 'write', None),
         ('calendar', 'read', None),   # all calendar reading
         ('calendar', 'write', 'OVERWRITE'),   # only overwriting calendar
         ('todo', 'read', None),     # all todo list reading
@@ -319,4 +466,16 @@ class Profile(com_samsung_packet.Profile):
         ('memo', 'read', None),     # all memo list reading
         ('memo', 'write', 'OVERWRITE'),  # all memo list writing
         )
+
+    __audio_ext={ 'MIDI': 'mid', 'PMD': 'pmd', 'QCP': 'qcp' }
+    def QueryAudio(self, origin, currentextension, afi):
+        # we don't modify any of these
+        print "afi.format=",afi.format
+        if afi.format in ("MIDI", "PMD", "QCP"):
+            for k,n in self.RINGTONE_LIMITS.items():
+                setattr(afi, k, n)
+            return currentextension, afi
+        d=self.RINGTONE_LIMITS.copy()
+        d['format']='QCP'
+        return ('qcp', fileinfo.AudioFileInfo(afi, **d))
 
