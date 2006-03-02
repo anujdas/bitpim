58c17d97252cf21895ccf883bdce926fa3cc7b34
skyjunky <skyjunky@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2006-03-02 09:37:11 +0000

    allow for new phone location
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2842 4404a10c-ff0d-0410-9031-a1dde6cb222d

---
+++ src/phones/__init__.py
@@ -0,0 +1,9 @@
+### BITPIM
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the BitPim license as detailed in the LICENSE file.
+
+### No code in this module
+
+
+
