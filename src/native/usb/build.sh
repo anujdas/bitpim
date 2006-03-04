8a11d7ceca61efa849d90fc0b44e8e7dd537105b
rogerb <rogerb@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2006-03-04 04:57:24 +0000

    add -fPIC to keep gentoo happy
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@2854 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/native/usb/build.sh
+++ src/native/usb/build.sh
@@ -3,5 +3,5 @@ INCLUDEDIR=`$PYTHONVER -c "import distutils.sysconfig; print distutils.sysconfig
 
 swig -python -I/usr/include libusb.i
 
-gcc -Wall -fno-strict-aliasing -O2 -g  -shared  -I $INCLUDEDIR -o _libusb.so libusb_wrap.c -lusb 
+gcc -fPIC -Wall -fno-strict-aliasing -O2 -g  -shared  -I $INCLUDEDIR -o _libusb.so libusb_wrap.c -lusb 
 strip _libusb.so
