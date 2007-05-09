ea270ae69a3af725fe76cb8093fe633ef19f98ca
djpham <djpham@4404a10c-ff0d-0410-9031-a1dde6cb222d>
2007-05-09 21:18:09 +0000

    (NH)Maintenance: Mac Build
    git-svn-id: svn://svn.code.sf.net/p/bitpim/code/trunk/bitpim@4224 4404a10c-ff0d-0410-9031-a1dde6cb222d

--- src/native/usb/macbuild.sh
+++ src/native/usb/macbuild.sh
@@ -3,7 +3,7 @@ INCLUDEDIR=/System/Library/Frameworks/Python.framework/Versions/2.3/include/$PYT
 
 swig -version 2>&1 | grep Version
 
-swig -python -I/usr/local/include libusb.i
+swig -python `libusb-config --cflags` libusb.i
 
-gcc -Wall -O2  -bundle -undefined suppress -flat_namespace -I $INCLUDEDIR -I /usr/local/include -o _libusb.so libusb_wrap.c -L/usr/local/lib -lusb -framework Python
+gcc -Wall -O2  -bundle -undefined suppress -flat_namespace -I $INCLUDEDIR -I /usr/local/include -o _libusb.so libusb_wrap.c `libusb-config --libs` `libusb-config --cflags` -framework Python
 
